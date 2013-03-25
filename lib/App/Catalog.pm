f2837e319e65b1c4d823919ebb24528617620e0e
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2013-03-25 09:18:40 +0100

    First draft of an input form for research data. Including lots of jQuery stuff and really cool form navigation.

--- lib/App/Catalog.pm
+++ lib/App/Catalog.pm
@@ -1,15 +1,40 @@
 package App::Catalog;
+
+use lib qw(/srv/www/sbcat/lib /srv/www/sbcat/lib/extension /srv/www/sbcat/lib/default);
 use Catmandu;
 use Catmandu::Sane;
 use Dancer ':syntax';
+use Catmandu::Store::MongoDB;
+use App::Import;
 
-sub store {
-  state $store = Catmandu->store;
-}
+#sub store {
+#  state $store = Catmandu->store;
+#}
+#
+#sub bag {
+#  state $bag = &store->bag;
+#}
 
-sub bag {
-  state $bag = &store->bag;
-}
+get '/submitForm' => sub {
+	my $params = params;
+	my $returnhash;
+	foreach my $key (keys %$params){
+		$returnhash->{parameters}->{$key} = $params->{$key};
+	}
+	
+	my $store = Catmandu::Store::MongoDB->new(database_name => 'bnbdb');
+	my $response = $store->bag->add($returnhash);
+	
+	template "backend/index.tt", {newRec => 'stored'};
+};
+
+get '/importId' => sub {
+	#my $documentId = params->{documentId} ||= "";
+	#my $idType = App::Import::identifyId($documentId);
+	
+	template "backend/header.tt";
+};
+
 
 get '/' => sub {
 	my $newRec;
@@ -17,9 +42,10 @@ get '/' => sub {
 	$newRec = params->{newRec} if params->{newRec};
 	
 	my $hits;
+	$hits->{parameters} = params;
 	$hits->{newRec} = $newRec if $newRec;
 	
-    template 'backend/index', $hits;
+    template 'backend/index.tt', $hits;
 };
 
-1;
+"The truth is out there";
--- public/javascripts/bootstrap.js
+++ public/javascripts/bootstrap.js
@@ -1495,15 +1495,15 @@
  * ======================================================== */
 
 
-!function ($) {
+/*$("#tabs-left,#tabs-right").show();*/
+!function( $ ){
 
-  "use strict"; // jshint ;_;
-
+  "use strict"
 
  /* TAB CLASS DEFINITION
   * ==================== */
 
-  var Tab = function (element) {
+  var Tab = function ( element ) {
     this.element = $(element)
   }
 
@@ -1517,7 +1517,6 @@
         , selector = $this.attr('data-target')
         , previous
         , $target
-        , e
 
       if (!selector) {
         selector = $this.attr('href')
@@ -1526,15 +1525,12 @@
 
       if ( $this.parent('li').hasClass('active') ) return
 
-      previous = $ul.find('.active:last a')[0]
+      previous = $ul.find('.active a').last()[0]
 
-      e = $.Event('show', {
-        relatedTarget: previous
+      $this.trigger({
+        type: 'show'
+      , relatedTarget: previous
       })
-
-      $this.trigger(e)
-
-      if (e.isDefaultPrevented()) return
 
       $target = $(selector)
 
@@ -1551,19 +1547,20 @@
       var $active = container.find('> .active')
         , transition = callback
             && $.support.transition
-            && $active.hasClass('fade')
+            && $active.is('.fade')
 
       function next() {
+          
         $active
-          .removeClass('active')
+          .removeClass('active in')
           .find('> .dropdown-menu > .active')
-          .removeClass('active')
+          .removeClass('active in')
 
         element.addClass('active')
 
         if (transition) {
-          element[0].offsetWidth // reflow for transition
-          element.addClass('in')
+            element[0].offsetWidth // reflow for transition
+            element.removeClass('out').addClass('in')
         } else {
           element.removeClass('fade')
         }
@@ -1577,9 +1574,9 @@
 
       transition ?
         $active.one($.support.transition.end, next) :
-        next()
+        next();
 
-      $active.removeClass('in')
+        $active.removeClass('out in');
     }
   }
 
@@ -1602,12 +1599,129 @@
  /* TAB DATA-API
   * ============ */
 
-  $(document).on('click.tab.data-api', '[data-toggle="tab"], [data-toggle="pill"]', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+  $(function () {
+    $('body').on('click.tab.data-api', '[data-toggle="tab"], [data-toggle="pill"]', function (e) {
+      e.preventDefault()
+      $(this).tab('show')
+    })
   })
 
-}(window.jQuery);/* =============================================================
+}( window.jQuery );
+
+//!function ($) {
+
+//  "use strict"; // jshint ;_;
+
+
+ /* TAB CLASS DEFINITION
+  * ==================== */
+
+//  var Tab = function (element) {
+//    this.element = $(element)
+//  }
+
+//  Tab.prototype = {
+
+//    constructor: Tab
+
+//  , show: function () {
+//      var $this = this.element
+//        , $ul = $this.closest('ul:not(.dropdown-menu)')
+//        , selector = $this.attr('data-target')
+//        , previous
+//        , $target
+//        , e
+
+//      if (!selector) {
+//        selector = $this.attr('href')
+//        selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
+//      }
+
+//      if ( $this.parent('li').hasClass('active') ) return
+
+//      previous = $ul.find('.active:last a')[0]
+
+//      e = $.Event('show', {
+//        relatedTarget: previous
+//      })
+
+//      $this.trigger(e)
+
+//      if (e.isDefaultPrevented()) return
+
+//      $target = $(selector)
+
+//      this.activate($this.parent('li'), $ul)
+//      this.activate($target, $target.parent(), function () {
+//        $this.trigger({
+//          type: 'shown'
+//        , relatedTarget: previous
+//        })
+//      })
+//    }
+
+//  , activate: function ( element, container, callback) {
+//      var $active = container.find('> .active')
+//        , transition = callback
+//            && $.support.transition
+//            && $active.hasClass('fade')
+
+//      function next() {
+//        $active
+//          .removeClass('active')
+//          .find('> .dropdown-menu > .active')
+//          .removeClass('active')
+
+//        element.addClass('active')
+
+//        if (transition) {
+//          element[0].offsetWidth // reflow for transition
+//          element.addClass('in')
+//        } else {
+//          element.removeClass('fade')
+//        }
+
+//        if ( element.parent('.dropdown-menu') ) {
+//          element.closest('li.dropdown').addClass('active')
+//        }
+
+//        callback && callback()
+//      }
+
+//      transition ?
+//        $active.one($.support.transition.end, next) :
+//        next()
+
+//      $active.removeClass('in')
+//    }
+//  }
+
+
+ /* TAB PLUGIN DEFINITION
+  * ===================== */
+
+//  $.fn.tab = function ( option ) {
+//    return this.each(function () {
+//      var $this = $(this)
+//        , data = $this.data('tab')
+//      if (!data) $this.data('tab', (data = new Tab(this)))
+//      if (typeof option == 'string') data[option]()
+//    })
+//  }
+
+//  $.fn.tab.Constructor = Tab
+
+
+ /* TAB DATA-API
+  * ============ */
+
+//  $(document).on('click.tab.data-api', '[data-toggle="tab"], [data-toggle="pill"]', function (e) {
+//    e.preventDefault()
+//    $(this).tab('show')
+//  })
+
+//}(window.jQuery);
+/* =============================================================
  * bootstrap-typeahead.js v2.2.1
  * http://twitter.github.com/bootstrap/javascript.html#typeahead
  * =============================================================
@@ -2021,5 +2135,6 @@
     })
   })
 
+  $('body').off('.tab.data-api');
 
 }(window.jQuery);
