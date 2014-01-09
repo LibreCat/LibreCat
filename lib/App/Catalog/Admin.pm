2934e9b42bd9dee387fe8bfebf43851270cb6c6a
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-01-09 16:39:07 +0100

    Added a temporary sub enabling /admin/[ID] URLs so that different user views can be displayed.

--- lib/App/Catalog/Admin.pm
+++ lib/App/Catalog/Admin.pm
@@ -2,11 +2,32 @@ package App::Catalog::Admin;
 
 use Catmandu::Sane;
 use Dancer ':syntax';
+use App::Catalog::Helper;
 
 prefix '/admin' => sub {
-
-	get '/' => sub {
-		template 'admin';
+    
+	get '/:id' => sub {
+		my $id = params->{id};
+		my $personInfo = h->getPerson($id);
+		my $sbcatId = $personInfo->{sbcatId};
+		
+		my $p = {
+			q => "person=$id AND hide<>$id",
+			facets => "",
+			limit => h->config->{store}->{maximum_page_size}
+		};
+		my $hits = h->search_publication($p);
+		
+		$hits->{sbcatId} = $sbcatId if $sbcatId;
+		$hits->{bisId} = $id;
+		my $personStyle;
+		my $personSort;
+		if($personInfo->{stylePreference} and $personInfo->{stylePreference} =~ /(\w{1,})\.(\w{1,})/){
+			$personStyle = $1;
+			$personSort = $2;
+		}
+		$hits->{style} = $personStyle || "pub";
+		template 'admin.tt', $hits;
 	};
 
 	get '/update' => sub {
@@ -19,6 +40,10 @@ prefix '/admin' => sub {
 
 	get '/curate' => sub {
 		template 'curate';
+	};
+	
+	get '/' => sub {
+		forward '/admin/86212';
 	};
 
 };
