e989b4972c7f575b3678e5135e308e290b68d878
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-01-13 16:13:04 +0100

    Had to remove prefix functionality (was faulty), added regexes to some subs and started on a search_researcher sub that ouputs json for remodeling of the Link to PEVZ Account function.

--- lib/App/Catalog/Admin.pm
+++ lib/App/Catalog/Admin.pm
@@ -2,54 +2,70 @@ package App::Catalog::Admin;
 
 use Catmandu::Sane;
 use Dancer ':syntax';
+use Dancer::Request;
 use App::Catalog::Helper;
-
-prefix '/admin' => sub {
     
-    get '/add' => sub {
-		template 'edit_researchData.tmpl', {recordOId => "123456789", file => [{fileOId => 1, fileName => "file", accessLevel => "admin", dateLastUploaded => "2014-01-10", isUploadedBy => {login => "kohorst"}}]};
-	};
+get '/admin' => sub {
+	forward '/admin/86212';
+};
 	
-	get '/:id' => sub {
-		my $id = params->{id};
-		my $personInfo = h->getPerson($id);
-		my $sbcatId = $personInfo->{sbcatId};
-		
-		my $p = {
-			q => "person=$id AND hide<>$id",
-			facets => "",
-			limit => h->config->{store}->{maximum_page_size}
-		};
-		my $hits = h->search_publication($p);
-		
-		$hits->{sbcatId} = $sbcatId if $sbcatId;
-		$hits->{bisId} = $id;
-		my $personStyle;
-		my $personSort;
-		if($personInfo->{stylePreference} and $personInfo->{stylePreference} =~ /(\w{1,})\.(\w{1,})/){
-			$personStyle = $1;
-			$personSort = $2;
-		}
-		$hits->{style} = $personStyle || "pub";
-		template 'admin.tt', $hits;
-	};
-
-	get '/update' => sub {
-		template 'admin_update';
-	};
-
-	get '/accounts' => sub {
-		template 'accounts'
-	};
-
-	get '/curate' => sub {
-		template 'curate';
-	};
+get '/admin/add' => sub {
+	template 'edit_researchData.tmpl', {recordOId => "123456789"};#, file => [{fileOId => 1, fileName => "file", accessLevel => "admin", dateLastUploaded => "2014-01-10", isUploadedBy => {login => "kohorst"}}]};
+};
 	
-	get '/' => sub {
-		forward '/admin/86212';
+get qr{/admin/(\d{1,})/*} => sub {
+	my ($id) = splat;
+	my $personInfo = h->getPerson($id);
+	my $sbcatId = $personInfo->{sbcatId};
+		
+	my $p = {
+		q => "person=$id AND hide<>$id",
+		facets => "",
+		limit => h->config->{store}->{maximum_page_size}
 	};
+	my $hits = h->search_publication($p);
+		
+	$hits->{sbcatId} = $sbcatId if $sbcatId;
+	$hits->{bisId} = $id;
+	my $personStyle;
+	my $personSort;
+	if($personInfo->{stylePreference} and $personInfo->{stylePreference} =~ /(\w{1,})\.(\w{1,})/){
+		$personStyle = $1;
+		$personSort = $2;
+	}
+	$hits->{style} = $personStyle || "pub";
+	template 'admin.tt', $hits;
+};
 
+get '/admin/update' => sub {
+	template 'admin_update';
+};
+
+get '/admin/accounts' => sub {
+	template 'accounts'
+};
+
+get '/admin/curate' => sub {
+	template 'curate';
+};
+	
+get '/admin/search_researcher' => sub {
+	my $q = params->{'q'};
+	my $hits = h->search_researcher({q => $q});
+		
+	to_dumper $hits;
+		
+		#my $jsonstring = "[";
+		#foreach (@{$hits->{hits}}){
+		#	my $mainTitle = $_->{mainTitle};
+		#	$mainTitle =~ s/"/\\"/g;
+		#	my $citation = $_->{citation}->{$style};
+		#	$citation =~ s/"/\\"/g;
+		#	$jsonstring .= "{oId:\"" . $_->{oId} . "\", title:\"" . $mainTitle . "\", citation:\"" . $citation . "\"},";
+		#}
+		#$jsonstring =~ s/,$//g;
+		#$jsonstring .= "]";
+		#return $jsonstring;
 };
 
 1;
