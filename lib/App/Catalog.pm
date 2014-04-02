240905a41cd07073a9e7d0ed48fe6ab07129cc1c
vpeil <vitali.peil@uni-bielefeld.de>
2014-04-02 14:03:13 +0200

    login/logout prototype

--- lib/App/Catalog.pm
+++ lib/App/Catalog.pm
@@ -17,10 +17,11 @@ use Authentication::Authenticate;
 
 Catmandu->load;
 
-hook_before => sub {
+hook 'before' => sub {
  	if( !session('user') && request->path_info !~ m{login} ) {
  		var requested_path => request->path_info;
- 		request->path_info('/login');
+ 		request->path_info('/myPUB/login');
+ 		#redirect '/myPUB/login';
  	}
 };
 
