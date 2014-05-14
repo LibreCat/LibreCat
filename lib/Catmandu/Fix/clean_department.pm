3bf5c184685d4f24924c575f1b32c7184dc58a2d
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-05-14 13:50:08 +0200

    New fix method for cleaning up departments.

---
+++ lib/Catmandu/Fix/clean_department.pm
@@ -0,0 +1,27 @@
+package Catmandu::Fix::clean_department;
+
+use Catmandu::Sane;
+use Moo;
+
+sub fix {
+	my ( $self, $pub ) = @_;
+	my $dep = $pub->{department};
+	
+	foreach (@$dep) {
+		delete $_->{oId} if $_->{oId};
+		delete $_->{type} if $_->{type};
+		
+		#ref $pub->{author} eq 'ARRAY' ? $pub->{author}->[0]->{fullName} : $pub->{author}->{fullName};
+		if ($_->{name}->[0]) {
+			$_->{name} = $_->{name}->[0]->{text};
+			$_->{id} = $_->{organizationNumber};
+			delete $_->{organizationNumber};
+			$_->{tree} = $_->{allDepartments};
+			delete $_->{allDepartments};
+		}
+	}
+	
+	$pub;
+}
+
+1;
