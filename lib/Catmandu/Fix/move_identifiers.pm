f06ec77f869ed8643f4ea49ae2a7bb23b10504a6
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-05-15 13:15:19 +0200

    Fixed external_id and publication_identifier hash layout.

--- lib/Catmandu/Fix/move_identifiers.pm
+++ lib/Catmandu/Fix/move_identifiers.pm
@@ -8,40 +8,38 @@ sub fix {
 	
 	if($pub->{issn} and ref $pub->{issn} eq "ARRAY"){
 		foreach my $issn (@{$pub->{issn}}){
-			push @{$pub->{publication_identifier}}, {issn => $issn};
+			push @{$pub->{publication_identifier}->{issn}}, $issn;
 		}
 	}
 	
 	if($pub->{eissn} and ref $pub->{eissn} eq "ARRAY"){
 		foreach my $eissn (@{$pub->{eissn}}){
-			push @{$pub->{publication_identifier}}, {eissn => $eissn};
+			push @{$pub->{publication_identifier}->{eissn}}, $eissn;
 		}
 	}
 	
 	if($pub->{isbn} and ref $pub->{isbn} eq "ARRAY"){
 		foreach my $isbn (@{$pub->{isbn}}){
-			push @{$pub->{publication_identifier}}, {isbn => $isbn};
+			push @{$pub->{publication_identifier}->{isbn}}, $isbn;
 		}
 	}
 	
 	push  @{$pub->{publication_identifier}}, {urn => $pub->{urn}} if $pub->{urn};
 	
 	
-	push @{$pub->{external_id}}, {isi => $pub->{isi}} if $pub->{isi};
-	push @{$pub->{external_id}}, {arxiv => $pub->{arxiv}} if $pub->{arxiv};
-	push @{$pub->{external_id}}, {pubmed => $pub->{medline}} if $pub->{medline};
-	push @{$pub->{external_id}}, {inspire => $pub->{inspire}} if $pub->{inspire};
-	push @{$pub->{external_id}}, {ahf => $pub->{ahf}} if $pub->{ahf};
-	push @{$pub->{external_id}}, {scoap3 => $pub->{scoap3}} if $pub->{scoap3};
-	push @{$pub->{external_id}}, {phillister => $pub->{phillister}} if $pub->{phillister};
-	push @{$pub->{external_id}}, {opac => $pub->{opac}} if $pub->{opac};
-	push @{$pub->{external_id}}, {fp7 => $pub->{fp7}} if $pub->{fp7};
-	push @{$pub->{external_id}}, {fp6 => $pub->{fp6}} if $pub->{fp6};
+	$pub->{external_id}->{isi} = {id => $pub->{isi}, prefix_id => "ISI:$pub->{isi}"} if $pub->{isi};
+	$pub->{external_id}->{arxiv} = {id => $pub->{arxiv}, prefix_id => "arXiv:$pub->{arxiv}"} if $pub->{arxiv};
+	$pub->{external_id}->{pmid} = {id => $pub->{medline}, prefix_id => "MEDLINE:$pub->{medline}"} if $pub->{medline};
+	$pub->{external_id}->{inspire} = {id=> $pub->{inspire}, prefix_id => "INSPIRE:$pub->{inspire}"} if $pub->{inspire};
+	$pub->{external_id}->{ahf} = {id => $pub->{ahf}, prefix_id => "AHF:$pub->{ahf}"} if $pub->{ahf};
+	$pub->{external_id}->{scoap3} = {id => $pub->{scoap3}, prefix_id => "SCOAP3:$pub->{scoap3}"} if $pub->{scoap3};
+	$pub->{external_id}->{phillister} = {id => $pub->{phillister}, prefix_id => "PhilLister:$pub->{phillister}"} if $pub->{phillister};
+	$pub->{external_id}->{opac} = {id => $pub->{opac}, prefix_id => "UB-OPAC:$pub->{opac}"} if $pub->{opac};
+	$pub->{external_id}->{fp7} = {id => $pub->{fp7}, prefix_id => $pub->{fp7}} if $pub->{fp7};
+	$pub->{external_id}->{fp6} = {id => $pub->{fp6}, prefix_id => $pub->{fp6}} if $pub->{fp6};
 	
-	
-	#genbank: []
-	#nasc: []
-	
+	$pub->{external_id}->{nasc} = $pub->{nascseedstockID} if $pub->{nascseedstockID};
+	$pub->{external_id}->{genbank} = $pub->{genbankID} if $pub->{genbankID};	
 	
 	$pub;
 }
