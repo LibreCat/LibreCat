a3d1965df64b60e20c6c72233dbcba88db10813e
vpeil <vitali.peil@uni-bielefeld.de>
2014-05-19 14:06:34 +0200

    more on controllers

---
+++ lib/App/Catalog/Controller/File.pm
@@ -0,0 +1,43 @@
+package App::Catalog::Controller::File;
+
+use Catmandu::Sane;
+use App::Catalog::Helper;
+use Exporter qw/import/;
+
+our @EXPORT = qw/new_file edit_file update_file delete_file/;
+
+my $upload_dir = h->config->{upload_dir};
+sub _create_id {
+    my $bag = h->bag->get('1');
+    my $id  = $bag->{"latest"};
+    $id++;
+    $bag = h->bag->add( { _id => "1", latest => $id } );
+    return $id;    # correct?
+}
+
+sub _bagit {
+	my $path = shift;
+	system "./bagit.py $path";
+}
+
+sub new_file {
+    return _create_id;
+}
+
+# maybe not needed, since all relevant information
+# are contained in the publication hash
+sub edit_file {}
+
+# this sub should be called from the sub 'update_publication',
+# if publication has a file attached
+sub update_file {
+	my $pub = shift;
+	my $dir = $upload_dir ."/$pub->{_id}";
+	system "/bin/mkdir $dir" unless -e $dir;
+}
+
+# this sub should be called from the sub 'delete_publication',
+# if publication has a file attached
+sub delete_file {}
+
+1;
