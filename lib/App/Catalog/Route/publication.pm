b0dd08ee3c9a6b6ded451c63290e83401ca56fcf
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-06-18 13:19:02 +0200

    File upload and editing (still needs work).

--- lib/App/Catalog/Route/publication.pm
+++ lib/App/Catalog/Route/publication.pm
@@ -100,11 +100,90 @@ post '/upload' => sub {
 		$return->{user} = session->{user};
 		$return->{size} = $file->{size};
 		$return->{file_id} = $file_id;
-		$return->{date} = "now";#???
+		$return->{date_updated} = "now";#???
 		$return->{access} = "openAccess";
 		$return->{content_type} = $file->{headers}->{"Content-Type"};
 		#$return->{error} = "this is not an error. Y U no display file?";
 		$return->{file_json} = '{"file_name": "'.$return->{filename}.'", "file_id": "'.$return->{file_id}.'", "content_type": "'.$return->{content_type}.'", "access_level": "'.$return->{access}.'", "date_updated": "'.$return->{date}.'", "date_created": "'.$return->{date}.'", "checksum": "ToDo", "file_size": "'.$return->{size}.'", "language": "eng", "creator": "'.$return->{user}.'", "open_access": "1", "year_last_uploaded": "2014"}';
+	}
+	else {
+		$return->{success} = 0;
+		$return->{error} = "There was an error while uploading your file."
+	}
+	#my $json = new JSON;
+	my $return_json = to_json($return);
+	return $return_json;
+};
+
+post '/upload/update' => sub {
+	my $file = request->upload('file');
+	my $old_file_name = params->{old_file_name};
+	my $id = params->{id};
+	my $file_id = params->{file_id};
+	my $success = 1;
+	
+	if($file){
+		# first delete old file from dir
+		my $deldir = h->config->{upload_dir} ."/$id/$old_file_name";
+		my $status = unlink "$deldir" if -e $deldir;
+		#return to_dumper $status;
+		
+		# then copy new file to dir
+		my $path = path(h->config->{upload_dir}, "$id", $file->{filename});
+		my $dir = h->config->{upload_dir}."/".$id;
+		mkdir $dir unless -e $dir;
+		$success = $file->copy_to($path);
+	}
+    
+    # then return data of updated file
+	my $return;
+	if($success){
+		$return->{success} = 1;
+		$return->{file_name} = $file ? $file->{filename} : $old_file_name;
+		$return->{creator} = session->{user};
+		$return->{file_size} = $file ? $file->{size} : "";
+		$return->{file_id} = $file_id;
+		$return->{date_updated} = "now";#???
+		$return->{access_level} = params->{access_level} ? params->{access_level} : "openAccess";
+		$return->{content_type} = $file ? $file->{headers}->{"Content-Type"} : "";
+		$return->{checksum} = "ToDo";
+		#$return->{error} = "this is not an error. Y U no display file?";
+		$return->{old_file_name} = $old_file_name;
+		
+		my $record = h->publication->get($id);
+		foreach my $recfile (@{$record->{file}}){
+			if($recfile->{file_id} eq $return->{file_id}){
+				$return->{year_last_uploaded} = $recfile->{year_last_uploaded};
+				$return->{file_size} = $recfile->{file_size} if $return->{file_size} eq "";
+				$return->{content_type} = ($return->{content_type} eq "" and $recfile->{content_type}) ? $recfile->{content_type} : "";
+				$return->{date_created} = $recfile->{date_created};
+				if($return->{access_level} eq "openAccess"){
+					$return->{open_access} = 1;
+				}
+				else {
+					$return->{open_access} = 0;
+				}
+				
+				$recfile = ();
+				$recfile = $return;
+				delete $recfile->{file_json};
+				delete $recfile->{success};
+				delete $recfile->{old_file_name};
+			}
+		}
+		
+		$return->{file_json} = '{"file_name": "'.$return->{file_name}.'", ';
+		$return->{file_json} .= '"file_id": "'.$return->{file_id}.'", ';
+		$return->{file_json} .= '"content_type": "'.$return->{content_type}.'", ';
+		$return->{file_json} .= '"access_level": "'.$return->{access_level}.'", ';
+		$return->{file_json} .= '"date_updated": "'.$return->{date_updated}.'", ';
+		$return->{file_json} .= '"date_created": "'.$return->{date_updated}.'", ';
+		$return->{file_json} .= '"checksum": "ToDo", "file_size": "'.$return->{file_size}.'", ';
+		$return->{file_json} .= '"language": "eng", "creator": "'.$return->{creator}.'", ';
+		$return->{file_json} .= '"open_access": "1", "year_last_uploaded": "2014"}';
+		
+		h->publication->add($record);
+		h->publication->commit;
 	}
 	else {
 		$return->{success} = 0;
