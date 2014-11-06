3667693a872a99b874cc4c1fc5047db32da034a2
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-11-06 13:51:09 +0100

    Fixes problem with ; in hash instead of ,
    Conflicts:
    	lib/App/Catalog/Route/file.pm

--- lib/App/Catalog/Route/file.pm
+++ lib/App/Catalog/Route/file.pm
@@ -9,16 +9,14 @@ package App::Catalog::Route::file;
 =cut
 
 use Catmandu::Sane;
+use Catmandu qw/export_to_string/;
 use Dancer ':syntax';
 use Dancer::Request;
 use App::Catalog::Helper;
+use App::Catalog::Controller::Permission qw/can_download/;
 use DateTime;
 use Try::Tiny;
 use Dancer::Plugin::Auth::Tiny;
-use App::AuthExtend;
-
-my $auth_extend = App::AuthExtend->new();
-Dancer::Plugin::Auth::Tiny->extend($auth_extend->extend);
 
 # some helpers
 ##############
@@ -47,29 +45,27 @@ prefix '/requestcopy' => sub {
     to the author.
 
 =cut
-	get '/requestcopy/:id/:file_id' => sub {
-		my $conf = h->config->{request_copy};
+	get '/:id/:file_id' => sub {
 		my $bag = 'x';
 
 		my $stored = $bag->add({
 			record_id => params->{id},
-			file_id => params->{id},
+			file_id => params->{file_id},
 			date_expires => calc_date,
 			email => params->{email},
 			});
-		my $key = $stored->{_id};
 
 		my $pub = edit_publication(params->{id});
-		my $mail_body =
-		"The publication '$pub->{title}' has been requested by params->{name} (params->{email}).\n
-		To approve this request click on the link below:\n\n" .
-		h->host ."/requestcopy/approve/" . $key ."\n\n
-		If you want to deny this request click on the following link:\n\n" .
-		h->host ."/requestcopy/deny/" . $key ."\n\n
-		Your PUB system";
+		my $mail_body = export_to_string({
+			title => $pub->{title},
+			user_name => params->{user_name},
+			key => $stored->{_id};
+		},
+			'Template',
+			template => 'email/req_copy.tt');
 		try {
 			email {
-				to => '',
+				to => 'todo',
 				subject => h->config->{request_copy}->{subject},
 				body => $mail_body,
 			};
@@ -89,10 +85,11 @@ prefix '/requestcopy' => sub {
 		my $data = $bag->get(params->{key});
 		$data->{approved} = 1;
 		$bag->add($data);
-		my $mail_body = "Your request has been approved by the author.\n\n
-		Please download the document within the next 10 days from\n"
-		. h->host ."requestcopy/download/$data->{_id}\n\n
-		Your PUB system";
+		my $mail_body = export_to_string({
+			key => param->{key}
+			},
+			'Template',
+			template => 'email/req_copy_approve.tt');
 		try {
 			email {
 				to => $data->{email},
@@ -114,13 +111,12 @@ prefix '/requestcopy' => sub {
 		my $bag = 'x';
 		my $data = $bag->get(params->{key});
 		$bag->delete(params->{key});
-		my $mail_body = "Your request has been denied by the author.\n\n
-		Your PUB system";
+		my $mail_body = export_to_string({}, 'Template', template => 'email/req_copy_refuse.tt');
 		try {
 			email {
 				to => $data->{email},
 				subject => h->config->{request_copy}->{subject},
-				body => $mail_body;
+				body => $mail_body,
 			};
 		} catch {
 			error "Could not send email: $_";
@@ -152,78 +148,86 @@ prefix '/requestcopy' => sub {
 =cut
 get '/download/:id/:file_id' => sub {
 
-	my $pub = h->publication->get(params->{id});
-	my $file_name;
-	my $access = "admin";
+	my ($ok, $file_name) = can_download(
+				params->{id},
+				params->{file_id},
+				session->{login},
+				session->{role},
+				request->address); # or maybe request->remote_host?
+	return '403' unless $ok;
 
-	foreach my $file (@{$pub->{file}}){
-		if($file->{file_id} eq params->{file_id}){
-			$access = $file->{access_level};
-			$file_name = $file->{file_name};
-			last;
-		}
-	}
-
-	# openAccess
-	if ($access eq 'openAccess') {
-		send_it(params->{id}, $file_name);
-	} elsif (exists session->{user} && session->{role} eq 'admin') {
-		send_it(params->{id}, $file_name);
-	}
-
-	# unibi
-	my $ip = request->{remote_adress};
-	if ($access eq 'unibi' && $ip =~ /^129.70/) {
-		send_it(params->{id}, $file_name);
-	}
-
-	# user/admin/reviewer
-	my $account = h->getAccount(session->{user})->[0];
-	my $role = session->{role};
-
-	if ($access eq 'admin' && ($role eq 'reviewer' || $role eq 'data_manager')) {
-
-		my $access_ok;
-		if($role eq "reviewer"){
-			foreach my $item (@{$account->{reviewer}}){
-				if(grep ($item, @{$pub->{department}})){
-					$access_ok = 1;
-				}
-			}
-		}
-		elsif($role eq "data_manager"){
-			if($pub->{type} eq "researchData" or $pub->{type} eq "dara"){
-				foreach my $item (@{$account->{department}}) {
-					if (grep ($item, @{$pub->{department}})) {
-						$access_ok = 1;
-					}
-				}
-			}
-		}
-
-
-		if ($access_ok) {
-			send_it(params->{id}, $file_name);
-		} else {
-			template 'error', {message => "Something went wrong. You don't have permission to see this document."};
-		}
-	} elsif ($access eq 'admin' && session->{role} eq 'user') {
-		my $access_ok;
-		foreach my $item (@{$pub->{author}}) {
-			if ($account->{_id} == $item->{id}) {
-				$access_ok = 1;
-			}
-		}
-
-		if ($access_ok) {
-			send_it(params->{id}, $file_name);
-		} else {
-			template 'error', {message => "Something went wrong. You don't have permission to see this document."};
-		}
-	} else {
-		template 'error', {message => "Something went wrong. You don't have permission to see this document."};
-	}
-
+	send_it(params->{id}, $file_name);
 };
 
 1;
+
+__END__
+	# my $access = "admin";
+	#
+	# foreach my $file (@{$pub->{file}}){
+	# 	if($file->{file_id} eq params->{file_id}){
+	# 		$access = $file->{access_level};
+	# 		$file_name = $file->{file_name};
+	# 		last;
+	# 	}
+	# }
+	#
+	# # openAccess
+	# if ($access eq 'openAccess') {
+	# 	send_it(params->{id}, $file_name);
+	# } elsif (exists session->{user} && session->{role} eq 'admin') {
+	# 	send_it(params->{id}, $file_name);
+	# }
+	#
+	# # unibi
+	# my $ip = request->{remote_adress};
+	# if ($access eq 'unibi' && $ip =~ /^129.70/) {
+	# 	send_it(params->{id}, $file_name);
+	# }
+	#
+	# # user/admin/reviewer
+	# my $account = h->getAccount(session->{user})->[0];
+	# my $role = session->{role};
+	#
+	# if ($access eq 'admin' && ($role eq 'reviewer' || $role eq 'data_manager')) {
+	#
+	# 	my $access_ok;
+	# 	if($role eq "reviewer"){
+	# 		foreach my $item (@{$account->{reviewer}}){
+	# 			if(grep ($item, @{$pub->{department}})){
+	# 				$access_ok = 1;
+	# 			}
+	# 		}
+	# 	}
+	# 	elsif($role eq "data_manager"){
+	# 		if($pub->{type} eq "researchData" or $pub->{type} eq "dara"){
+	# 			foreach my $item (@{$account->{department}}) {
+	# 				if (grep ($item, @{$pub->{department}})) {
+	# 					$access_ok = 1;
+	# 				}
+	# 			}
+	# 		}
+	# 	}
+	#
+	#
+	# 	if ($access_ok) {
+	# 		send_it(params->{id}, $file_name);
+	# 	} else {
+	# 		template 'error', {message => "Something went wrong. You don't have permission to see this document."};
+	# 	}
+	# } elsif ($access eq 'admin' && session->{role} eq 'user') {
+	# 	my $access_ok;
+	# 	foreach my $item (@{$pub->{author}}) {
+	# 		if ($account->{_id} == $item->{id}) {
+	# 			$access_ok = 1;
+	# 		}
+	# 	}
+	#
+	# 	if ($access_ok) {
+	# 		send_it(params->{id}, $file_name);
+	# 	} else {
+	# 		template 'error', {message => "Something went wrong. You don't have permission to see this document."};
+	# 	}
+	# } else {
+	# 	template 'error', {message => "Something went wrong. You don't have permission to see this document."};
+	# }
