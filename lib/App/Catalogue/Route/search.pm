ea984d0eafe9488cf669cafce6357947f0ce4e02
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2014-12-16 13:19:32 +0100

    New tab "data publications" to make backend view look even more like person pages.

--- lib/App/Catalogue/Route/search.pm
+++ lib/App/Catalogue/Route/search.pm
@@ -153,6 +153,8 @@ prefix '/myPUB/search' => sub {
         my $p = h->extract_params();
         my $id = session 'personNumber';
         push @{$p->{q}}, "(person=$id OR creator=$id)";
+        push @{$p->{q}}, "type<>researchData";
+        push @{$p->{q}}, "type<>dara";
         (params->{text} =~ /^".*"$/) ? (push @{$p->{q}}, params->{text}) : (push @{$p->{q}}, '"'.params->{text}.'"') if params->{text};
 
         $p->{facets} = h->default_facets();
@@ -184,9 +186,38 @@ prefix '/myPUB/search' => sub {
     };
 
     get '/data' => sub {
+    	my $p = h->extract_params();
+        my $id = session 'personNumber';
+        push @{$p->{q}}, "(person=$id OR creator=$id)";
+        push @{$p->{q}}, "(type=researchData OR type=dara)";
+        (params->{text} =~ /^".*"$/) ? (push @{$p->{q}}, params->{text}) : (push @{$p->{q}}, '"'.params->{text}.'"') if params->{text};
 
+        $p->{facets} = h->default_facets();
+        my $sort_style = h->get_sort_style( $p->{sort} || '', $p->{style} || '');
+        $p->{sort} = $sort_style->{sort_backend};
+
+        # override default author/editor facet
+        $p->{facets}->{author} = {
+            terms => {
+                field   => 'author.id',
+                size    => 20,
+                exclude => [$id]
+            }
+        };
+        $p->{facets}->{editor} = {
+            terms => {
+                field   => 'editor.id',
+                size    => 20,
+                exclude => [$id]
+            }
+        };
+
+        my $hits = h->search_publication($p);
+        $hits->{style} = $sort_style->{style};
+        $hits->{sort} = $p->{sort};
+        $hits->{modus} = "data";
+        template "home", $hits;
     };
-
 };
 
 1;
