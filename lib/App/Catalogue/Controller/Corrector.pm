533a104d84974a70334f300ea0213ac6eeaa9ca3
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2015-01-21 08:38:46 +0100

    Skips publication_identifier field when correcting hash/array.

--- lib/App/Catalogue/Controller/Corrector.pm
+++ lib/App/Catalogue/Controller/Corrector.pm
@@ -45,6 +45,7 @@ sub correct_hash_array {
 
 	foreach my $key (keys %$data){
 		next if ($key eq "related_material");
+		next if ($key eq "publication_identifier");
 		my $ref = ref $data->{$key};
 		my $fields_tab = $fields->{basic_fields}->{$key} || $fields->{file_upload}->{$key} || $fields->{supplementary_fields}->{$key} || $fields->{related_material}->{$key};
 
