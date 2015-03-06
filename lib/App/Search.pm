d37d101414c48e4323583ccb4fb0893266a77a81
vpeil <vitali.peil@uni-bielefeld.de>
2015-03-06 10:31:40 +0100

    activate project route

--- lib/App/Search.pm
+++ lib/App/Search.pm
@@ -6,14 +6,14 @@ use App::Helper;
 
 use App::Search::Route::api;
 #use App::Search::Route::award;
-use App::Search::Route::person;
+use App::Search::Route::directoryindex;
 use App::Search::Route::mark;
-#use App::Search::Route::project;
+use App::Search::Route::person;
+use App::Search::Route::project;
 use App::Search::Route::publication;
 use App::Search::Route::thumbnail;
 use App::Search::Route::uri;
 use App::Search::Route::unapi;
-use App::Search::Route::directoryindex;
 
 get '/' => sub {
     template '/websites/index_publication.tt', {bag => "home"};
