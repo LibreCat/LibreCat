82832a21940aebbf5ccbdd90e1d4a94e6d9f5241
bup <bup@pub.unibi.de>
2015-03-11 15:35:49 +0100

    Adds glyphicons to mark/unmark links even after js modification.

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -4,24 +4,24 @@ $(function () {
 		var a = $(this);
 		var marked = a.data('marked');
 		if (marked == 0) {
-			$('div.unmark_all').empty().append('<a class="unmark-all" href="#">Unmark all</a>');
+			$('div.unmark_all').empty().append('<a class="unmark-all" href="#"><span class="glyphicon glyphicon-remove"></span>Unmark all</a>');
 			$('a.unmark-all').bind("click", function() {
 			    evt.preventDefault();
 		        $.post('/marked?x-tunneled-method=DELETE', function(res) {
-		          $('a.mark').data('marked', 0).text('Mark');
+		          $('a.mark').data('marked', 0).html('<span class="glyphicon glyphicon-ok"></span>Mark');
 		          $('.unmark_all').html('');
 		          $('.total-marked').text(res.total);
 		        }, 'json');
 		      });
 			$.post('/mark/'+a.data('id'), function(res) {
 				$('.total-marked').text(res.total);
-				a.data('marked', 1).text('Unmark');
+				a.data('marked', 1).html('<span class="glyphicon glyphicon-remove"></span>Unmark');
 			}, 'json');
 		}
 		else {
 			$.post('/mark/'+a.data('id')+'?x-tunneled-method=DELETE', function(res) {
 				$('.total-marked').text(res.total);
-				a.data('marked', 0).text('Mark');
+				a.data('marked', 0).html('<span class="glyphicon glyphicon-ok"></span>Mark');
 			}, 'json');
 			if($('.total-marked').text() == "1") {
 				$('div.unmark_all').empty();
