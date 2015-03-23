79a3afcbb2e144ab2bf5eb279fafe01d7f3c3e5c
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2015-03-23 17:08:39 +0100

    Fixes #81 : Link zu Datei aus dropzone-Element entfernt, wird erst angezeigt, nachdem der Rec gespeichert und die Datei in den korrekten download-Ordner verschoben wurde.

--- public/javascripts/pub_dropzones.js
+++ public/javascripts/pub_dropzones.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
 	            if(resp.success){
 	              $(file.previewElement).addClass("alert alert-success");
 	            	
-	              var fileName = Dropzone.createElement("<div class=\"row\"><div class=\"col-md-12 padded\" id=\"filename_" + resp.tempid + "\"><span class=\"glyphicon glyphicon-file text-muted\"></span> <a href=\"[% h.shost %]/download/" + resp.file_name + "\" target=\"_blank\">" + file.name + "</a></div></div>");
+	              var fileName = Dropzone.createElement("<div class=\"row\"><div class=\"col-md-12 padded text-muted\" id=\"filename_" + resp.tempid + "\"><span class=\"glyphicon glyphicon-file text-muted\"></span> <strong>" + file.name + "</strong></div></div>");
 	              file.previewElement.appendChild(fileName);
 	              
 	              var tagsRow = Dropzone.createElement("<div class=\"row\"><div class=\"col-md-2 text-muted\">Access Level:</div><div class=\"col-md-3 text-muted\">Upload Date:</div><div class=\"col-md-3 text-muted\">User:</div><div class=\"col-md-4 text-muted\">Relation:</div></div>");
