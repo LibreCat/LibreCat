ffd740a7672386b492e2c4d1df5a361d0357512c
vpeil <vitali.peil@uni-bielefeld.de>
2015-03-30 10:18:19 +0200

    refactoring by introducing fixes

---
+++ lib/Catmandu/Fix/hash_array.pm
@@ -0,0 +1,35 @@
+package Catmandu::Fix::hash_array;
+
+use Catmandu::Sane;
+use Catmandu::Util qw(:array);
+use App::Helper;
+use Moo;
+
+sub fix {
+    my ($self, $data) = @_;
+
+    my $conf = h->config;
+    my $tmp_type;
+    if ($data->{type} =~ /^bi[A-Z]/) {
+        $tmp_type = "bithesis";
+    } else {
+        $tmp_type = lc($data->{type});
+    }
+	my $fields = $conf->{forms}->{publicationTypes}->{$tmp_type}->{fields};
+
+	foreach my $key (keys %$data){
+		my $ref = ref $data->{$key};
+		my $array_field = $conf->{forms}->{array_field};
+
+		if($ref ne "ARRAY" and array_includes($array_field,$key)){
+			$data->{$key} = [$data->{$key}];
+		}
+		if($ref eq "ARRAY" and !array_includes($array_field,$key)){
+			$data->{$key} = $data->{$key}->[0];
+		}
+	}
+
+	return $data;
+}
+
+1;
