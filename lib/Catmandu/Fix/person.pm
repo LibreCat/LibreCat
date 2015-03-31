b82ce53cc0474e9022990367e6a4bb70ca6846fe
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2015-03-31 08:10:08 +0200

    Fix to remove empty author/editor/supervisor/translator fields before a filled-out one.

--- lib/Catmandu/Fix/person.pm
+++ lib/Catmandu/Fix/person.pm
@@ -9,11 +9,26 @@ sub fix{
 
     foreach my $pers (@p) {
         next unless $data->{$pers};
-        map {
-            if ($_->{first_name} and $_->{last_name}) {
-                $_->{full_name} = $_->{last_name} . ", " .$_->{first_name};
-            }
-        } @{$data->{$pers}};
+        
+        @{$data->{$pers}} = grep defined, @{$data->{$pers}};
+        my $splice_me;
+    	my $i = 0;
+        
+        foreach (@{$data->{$pers}}){
+        	if ($_->{first_name} and $_->{last_name}) {
+        		$_->{full_name} = $_->{last_name} . ", " . $_->{first_name};
+   				$i++;
+   			}
+   			else {
+   				push @$splice_me, $i;
+   			}
+        }
+        
+        if($splice_me){
+   			foreach (@$splice_me){
+   				splice @{$data->{$pers}}, $_, 1;
+   			}
+   		}
     }
     return $data;
 }
