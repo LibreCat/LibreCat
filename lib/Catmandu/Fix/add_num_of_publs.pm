353d26d80856f87d598698a280964871a292b252
vpeil <vitali.peil@uni-bielefeld.de>
2015-06-02 10:49:02 +0200

    cleanup + refactored

--- lib/Catmandu/Fix/add_num_of_publs.pm
+++ lib/Catmandu/Fix/add_num_of_publs.pm
@@ -1,18 +1,15 @@
 package Catmandu::Fix::add_num_of_publs;
 
-use lib qw(/srv/www/pub);
-
-use Catmandu;
 use Catmandu::Sane;
+use Catmandu -load;
 use Moo;
 
 Catmandu->load(':up');
-my $bag = Catmandu->store('search')->bag('publication');
-#my $researchBag = Catmandu->store('search')->bag('researchData');
 
 sub fix {
-    my ( $self, $rec ) = @_;
+    my ($self, $rec) = @_;
 
+    my $bag = Catmandu->store('search')->bag('publication');
     if($rec->{_id}){
     	my $hits = $bag->search(
 		  cql_query => "person=$rec->{_id} AND status=public AND type<>researchData AND type<>dara",
