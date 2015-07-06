c7b9819c40b0c0b1c440a40ef9c4f56b167116fe
vpeil <vitali.peil@uni-bielefeld.de>
2015-07-06 14:26:12 +0200

    bug fix: departments

--- lib/Catmandu/Fix/add_department.pm
+++ lib/Catmandu/Fix/add_department.pm
@@ -1,19 +1,22 @@
 package Catmandu::Fix::add_department;
 
 use Catmandu::Sane;
-use App::Helper;
-use Dancer qw(:syntax);
 use Moo;
+use App::Helper;
+use Dancer qw(:syntax session);
 
 sub fix {
     my ($self, $data) = @_;
 
     unless ($data->{department}) {
         my $person = h->get_person( session->{personNumber} );
-        @{$data->{department}} = map {
-            $_->{id};
-        } @{$person->{department}};
+        $data->{department} = $person->{department};
+        foreach my $d (@{$data->{$department}}) {
+            $d->{tree} = h->get_department($d->{id})->{tree};
+        }
     }
+
+    $data;
 }
 
 1;
