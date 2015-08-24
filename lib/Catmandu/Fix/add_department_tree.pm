37e302a1aca51f01d4cac0083b275ed7541b0e78
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2015-08-24 15:51:57 +0200

    new fix method to correct department->tree entries coming from live system

---
+++ lib/Catmandu/Fix/add_department_tree.pm
@@ -0,0 +1,27 @@
+package Catmandu::Fix::add_department_tree;
+
+use Catmandu::Sane;
+use Moo;
+use App::Helper;
+use Dancer qw(:syntax session);
+
+sub fix {
+    my ($self, $data) = @_;
+
+    foreach my $d (@{$data->{department}}) {
+    	my $dep;
+    	if($d->{id} !~ /\d{1,}/){
+    		$dep = h->get_department($d->{name});
+    	}
+    	else {
+    		$dep = h->get_department($d->{id});
+    	}
+        
+        $d->{tree} = ();
+        $d->{tree} = $dep->{tree} if ($dep and $dep->{tree});
+    }
+
+    $data;
+}
+
+1;
