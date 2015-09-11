30590797370f284c3efc3fdbc885b3a54939f997
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2015-09-11 13:33:23 +0200

    Modifications to department_tree fix, accomodating the new full department structure coming from the department index.

--- lib/Catmandu/Fix/add_department_tree.pm
+++ lib/Catmandu/Fix/add_department_tree.pm
@@ -10,15 +10,25 @@ sub fix {
 
     foreach my $d (@{$data->{department}}) {
     	my $dep;
-    	if($d->{_id} !~ /\d{1,}/){
-    		$dep = h->get_department($d->{name});
+    	if(!$d->{_id} and $d->{id}){
+    		$d->{_id} = $d->{id};
+    		delete $d->{id};
+    	}
+    	if(!$d->{_id} or $d->{_id} !~ /\d{1,}/){
+    		#$dep = h->get_department($d->{name});
+    		$dep = h->search_department({q => ["display=\"$d->{name}\""]})->{hits}->[0];
     	}
     	else {
-    		$dep = h->get_department($d->{_id});
+    		#$dep = h->get_department($d->{_id});
+    		$dep = h->search_department({q => [$d->{_id}]})->{hits}->[0];
     	}
         
-        $d->{tree} = ();
-        $d->{tree} = $dep->{tree} if ($dep and $dep->{tree});
+        delete $dep->{date_created};
+        delete $dep->{_version};
+        delete $dep->{date_updated};
+        $d = $dep;
+        #$d->{tree} = ();
+        #$d->{tree} = $dep->{tree} if ($dep and $dep->{tree});
     }
 
     $data;
