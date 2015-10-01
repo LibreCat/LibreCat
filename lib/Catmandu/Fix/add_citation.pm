d600960ed6f5dae0f31873e856974c0be2c481cf
vpeil <vitali.peil@uni-bielefeld.de>
2015-10-01 08:07:46 +0200

    bug fix

--- lib/Catmandu/Fix/add_citation.pm
+++ lib/Catmandu/Fix/add_citation.pm
@@ -2,20 +2,18 @@ package Catmandu::Fix::add_citation;
 
 use Catmandu::Sane;
 use Catmandu qw(export_to_string);
+use Catmandu::Fix::clone as => 'clone';
 use Citation;
 use Moo;
 
 
-# TODO: make path configurable
-
-my $conf = Catmandu->config->{citation};
-
 sub fix {
     my ($self, $data) = @_;
 
-    $data->{citation} = Citation->new(all => 1)->create($data);
-
-    return $data;
+    my $d = clone $data;
+    $data->{citation} =  Citation->new(all => 1)->create($d);
+    
+    $data;
 }
 
 1;
