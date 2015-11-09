4f4d25cdf2c79ccd3ecae5bbfb860bfaf13d0024
Vitali Peil <vitali.peil@uni-bielefeld.de>
2015-11-09 13:29:51 +0100

    another worker: datacite registry, push datacite xml and mint DOIs

---
+++ lib/LibreCat/Worker/DataCite.pm
@@ -0,0 +1,117 @@
+package LibreCat::Worker::DataCite;
+
+use Moo;
+use LWP;
+use Crypt::SSLeay;
+use Term::ReadKey;
+use URI;
+use URI::Escape;
+use Encode qw( encode_utf8 );
+use namespace::clean;
+
+with 'LibreCat::Worker';
+
+has base_url => (is => 'ro', default => sub {'https://mds.datacite.org'});
+has user => (is => 'ro', required => 1);
+has password => (is => 'ro', required => 1);
+has test_mode => (is => 'ro');
+
+sub do_work {
+    my ($self, $doi, $landing_url, $datacite_xml) = @_;
+
+    $self->metadata($doi, $datacite_xml);
+    $self->mint($doi, $landing_url);
+}
+
+sub mint {
+    my ($self, $doi, $landing_url) = @_;
+
+    $self->log->debug("Minting $doi to $landing_url.");
+
+    $self->_do_request(
+        'POST',
+        $self->base_url ."/doi",
+        "doi=$doi\nurl=$landing_url",
+        'text/plain;charset=UTF-8',
+    );
+}
+
+sub metadata {
+    my ($self, $doi, $datacite_xml) = @_;
+
+    $self->log->debug("Register metadata for $doi. XML: $datacite_xml.");
+
+    $self->_do_request(
+        'POST',
+        $self->base_url ."/metadata",
+        $datacite_xml,
+        'application/xml;charset=UTF-8',
+    );
+}
+
+sub _do_request {
+    my ($self, $method, $url, $content, $content_type) = @_;
+
+    $content .= "\ntestMode=true" if $self->test_mode;
+    my $headers = HTTP::Headers->new(
+        Accept => 'application/xml',
+        'Content-Type' => $content_type,
+    );
+
+    my $req = HTTP::Request->new(
+        $method => $url,
+        $headers, encode_utf8($content)
+    );
+
+    $self->log->debug("Sending $method request to $url.");
+
+    $req->authorization_basic( $self->user, $self->password );
+    my $ua = LWP::UserAgent->new;
+    my $res = $ua->request($req);
+
+    my $status = $res->code();
+    $self->log->debug("Status code $status.");
+    return $status;
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Worker::DataCite - a worker for registering and minting at DataCite
+
+=head2 SYNOPSIS
+
+    use LibreCat::Worker::DataCite;
+
+    my $registry = LibreCat::Worker::DataCite->new(user => 'me', password => 'secret');
+
+    $registry->do_work()
+
+    # or
+    $registry->metadata()
+    $registry->mint('')
+
+=head2 CONFIGURATION
+
+=over
+
+=item user
+
+Required.
+
+=item password
+
+Required.
+
+=back
+
+=head2 SEE ALSO
+
+L<LibreCat::Worker>
+
+=cut
