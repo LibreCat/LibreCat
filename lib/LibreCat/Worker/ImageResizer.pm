280ec6aee705a64d4cbb40d929dc788a90eda116
Vitali Peil <vitali.peil@uni-bielefeld.de>
2015-11-10 11:20:15 +0100

    pod

--- lib/LibreCat/Worker/ImageResizer.pm
+++ lib/LibreCat/Worker/ImageResizer.pm
@@ -11,27 +11,49 @@ has input_file => (is => 'ro', required => 1);
 has output_file => (is => 'ro');
 has width => (is => 'ro', default => sub {200});
 has height => (is => 'ro', default => sub {200});
-#has image_resizer => (is => 'lazy');
-
-sub _build_image_resizer {
-    my ($self) = @_;
-
-    my $resizer = Image::Resizer->new($self->input_file);
-
-}
 
 sub do_work {
     my ($self) = @_;
 
     my $file_path = path(h->get_file_path($id), $file_name);
     my $thumbnail = path(dirname($file_path), "thumbnail.png");
+
+    $self->log->debug("Converting $file_path to ".$self->height ."x".$self->width);
+
     unless (-e $thumbnail) {
-        system "convert -resize x200 ${file_path}[0] $thumbnail";
+        my $exit_code = system "convert -resize x200 ${file_path}[0] $thumbnail";
     }
 
-#    my $gd = $self->image_resizer->resize($self->width, $self->height);
-
-
+    $self->log->error("Error $!") unless $exit_code eq '0'.
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Worker::ImageResizer
+
+=head2 SYNOPSIS
+
+    use LibreCat::Worker::ImageResizer;
+
+    my $resizer = LibreCat::Worker::ImageResizer->new(
+        input_file => 'data/uploads/paper.pdf',
+        output_file => 'thumbnail.png',
+        height => 200,
+        widht => 120,
+    );
+
+    $resizer->do_work;
+
+=head2 CONFIGURATION
+
+=head2 SEE ALSO
+
+L<LibreCat::Worker>
+
+=cut
