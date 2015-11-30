6a7cbae2749d3cbbdeb1fd94bbb6b5a9704c145e
Vitali Peil <vitali.peil@uni-bielefeld.de>
2015-11-30 10:37:19 +0100

    epmc importer: bug fix

--- lib/Catmandu/Fix/split_author.pm
+++ lib/Catmandu/Fix/split_author.pm
@@ -14,6 +14,8 @@ sub fix {
             foreach my $a (@$au) {
                 if ($a =~ /(\w+.*),\s(\w+.*)/) {
                     push @{$pub->{$entity}}, {full_name => $a, first_name => $2, last_name => $1};
+                } elsif ($a =~ /(\w+.*?)\s([A-Z]{1,2})(\sJr|\sSr)*$/) {
+                    push @{$pub->{$entity}}, {full_name => "$1, $2", first_name => $2, last_name => $1};
                 } elsif ($a =~ /(\w+.*?)\s(\w+.*)$/) {
                     push @{$pub->{$entity}}, {full_name => "$2, $1", first_name => $1, last_name => $2};
                 }
