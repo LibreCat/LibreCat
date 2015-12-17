721a10bdaac9de6c3bbca370c3ee72fc576e9556
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2015-12-17 15:02:19 +0000

    Add protection for no session available environments

--- lib/Catmandu/Fix/add_department.pm
+++ lib/Catmandu/Fix/add_department.pm
@@ -8,13 +8,19 @@ use Dancer qw(:syntax session);
 sub fix {
     my ($self, $data) = @_;
 
-    unless ($data->{department} and session->{personNumber}) {
-        my $person = h->get_person( session->{personNumber} );
-        $data->{department} = $person->{department};
-        foreach my $d (@{$data->{department}}) {
-            $d->{tree} = h->get_department($d->{_id})->{tree};
-        }
-    }
+	eval {
+	    unless ($data->{department} and session->{personNumber}) {
+	        my $person = h->get_person( session->{personNumber} );
+	        $data->{department} = $person->{department};
+	        foreach my $d (@{$data->{department}}) {
+	            $d->{tree} = h->get_department($d->{_id})->{tree};
+	        }
+	    }
+	};
+
+	if ($@) {
+		# no nothing when we don't have a session
+	}
 
     $data;
 }
