d69055b2baaa2ce8034acf246b661f42d00a48ac
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2015-12-17 12:37:57 +0000

    Indent

--- bin/user_admin.pl
+++ bin/user_admin.pl
@@ -26,13 +26,13 @@ if ($cmd eq 'list') {
     $ret = cmd_list(@ARGV);
 }
 elsif ($cmd eq 'add') {
-	$ret = cmd_add(@ARGV);
+    $ret = cmd_add(@ARGV);
 }
 elsif ($cmd eq 'get') {
-	$ret = cmd_get(@ARGV);
+    $ret = cmd_get(@ARGV);
 }
 elsif ($cmd eq 'delete') {
-	$ret = cmd_delete(@ARGV);
+    $ret = cmd_delete(@ARGV);
 }
 else {
     print STDERR "unknown command - $cmd\n";
@@ -42,82 +42,82 @@ else {
 exit($ret);
 
 sub cmd_list {
-	my $count = $h->researcher->each(sub {
-		my ($item) = @_;
-		my $id       = $item->{_id};
-		my $login    = $item->{login};
-		my $name     = $item->{full_name};
-		my $status   = $item->{account_status};
-		my $type     = $item->{account_type};
-		my $is_admin = $item->{super_admin};
+    my $count = $h->researcher->each(sub {
+        my ($item) = @_;
+        my $id       = $item->{_id};
+        my $login    = $item->{login};
+        my $name     = $item->{full_name};
+        my $status   = $item->{account_status};
+        my $type     = $item->{account_type};
+        my $is_admin = $item->{super_admin};
 
-		printf "%s %5d %-20.20s %-40.40s %-10.10s %s\n" 
-					, $is_admin ? "*" : " "
-					, $id
-					, $login
-					, $name
-					, $status
-					, $type; 
-	});
-	print "count: $count\n";
-	
-	return 0;
+        printf "%s %5d %-20.20s %-40.40s %-10.10s %s\n" 
+                    , $is_admin ? "*" : " "
+                    , $id
+                    , $login
+                    , $name
+                    , $status
+                    , $type; 
+    });
+    print "count: $count\n";
+    
+    return 0;
 }
 
 sub cmd_get {
-	my ($id) = @_;
+    my ($id) = @_;
 
-	croak "usage: $0 get <id>" unless defined($id);
+    croak "usage: $0 get <id>" unless defined($id);
 
-	my $data = $h->get_person($id);
+    my $data = $h->get_person($id);
 
-	Catmandu->export($data, 'YAML') if $data;
+    Catmandu->export($data, 'YAML') if $data;
 
-	return $data ? 0 : 2;
+    return $data ? 0 : 2;
 }
 
 sub cmd_add {
-	my ($file) = @_;
+    my ($file) = @_;
 
-	croak "usage: $0 add <FILE>" unless defined($file) && -r $file;
+    croak "usage: $0 add <FILE>" unless defined($file) && -r $file;
 
-	my $data = Catmandu::Util::read_yaml($file);
+    my $data = Catmandu::Util::read_yaml($file);
 
-	my $validator = App::Validator::Researcher->new;
+    my $validator = App::Validator::Researcher->new;
 
-	if ($validator->is_valid($data)) {
-		my $result = $h->update_record('researcher', $data);
-		if ($result) {
-			print "added " . $data->{_id} . "\n";
-			return 0;
-		}
-		else {
-			print "ERROR: add " . $data->{_id} . " failed\n";
-			return 2; 
-		}
-	}
-	else {
-		print STDERR "ERROR: not a valid researcher\n";
-		print STDERR join("\n",@{$validator->last_errors}) , "\n";
-		return 2;
-	}
+    if ($validator->is_valid($data)) {
+        my $result = $h->update_record('researcher', $data);
+        if ($result) {
+            print "added " . $data->{_id} . "\n";
+            return 0;
+        }
+        else {
+            print "ERROR: add " . $data->{_id} . " failed\n";
+            return 2; 
+        }
+    }
+    else {
+        print STDERR "ERROR: not a valid researcher\n";
+        print STDERR join("\n",@{$validator->last_errors}) , "\n";
+        return 2;
+    }
 }
 
 sub cmd_delete {
-	my ($id) = @_;
+    my ($id) = @_;
 
-	croak "usage: $0 delete <id>" unless defined($id);
+    croak "usage: $0 delete <id>" unless defined($id);
 
-	my $result = $h->researcher->delete($id);
+    my $result = $h->researcher->delete($id);
 
-	if ($h->researcher->commit) {
-		print "deleted $id\n";
-		return 0;
-	}
-	else {
-		print STDERR "ERROR: delete $id failed";
-		return 2;
-	}
+    if ($h->researcher->commit) {
+        print "deleted $id\n";
+        return 0;
+    }
+    else {
+        print STDERR "ERROR: delete $id failed";
+        return 2;
+    }
 }
 
 sub usage {
