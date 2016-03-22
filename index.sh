19008270067fe8f1fba3ff562041ddf9b5d53e8b
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2016-03-22 12:47:34 +0000

    Creating import and export options for the default index

--- index.sh
+++ index.sh
@@ -13,8 +13,11 @@ case "${CMD}" in
 	;;
    drop)
 	echo "Dropping index.."
+	echo "researcher..."
 	carton exec catmandu delete search --bag researcher
-	carton exec catmandu delete search --bag publications
+	echo "publication..."
+	carton exec catmandu delete search --bag publication
+	echo "department..."
 	carton exec catmandu delete search --bag department
 	echo "Done"
 	;;
@@ -23,8 +26,11 @@ case "${CMD}" in
 
 	mkdir -p ${TMPDIR}
 
+	echo "researcher..."
 	carton exec catmandu export search --bag researcher  to YAML > ${TMPDIR}/researcher.yml
+	echo "publication..."
 	carton exec catmandu export search --bag publication to YAML > ${TMPDIR}/publications.yml
+	echo "department..."
 	carton exec catmandu export search --bag department to YAML > ${TMPDIR}/department.yml
 
 	cd ${TMPDIR}
@@ -40,6 +46,7 @@ case "${CMD}" in
 	echo "Done"
 	;;
    import)
+	echo "Importing index..."
     FILE=$2
 
     if [ "${FILE}" == "" ]; then
@@ -48,6 +55,24 @@ case "${CMD}" in
     	exit 1
     fi
 
+	mkdir -p ${TMPDIR}
+	
+	cd ${TMPDIR}
+
+	unzip ${FILE}
+
+	cd -
+
+	echo "researcher..."
+	carton exec catmandu import YAML to search --bag researcher < ${TMPDIR}/researcher.yml
+	echo "publications..."
+	carton exec catmandu import YAML to search --bag publication < ${TMPDIR}/publications.yml
+	echo "department..."
+	carton exec catmandu import YAML to search --bag department < ${TMPDIR}/department.yml
+
+	rm -rf ${TMPDIR}
+
+	echo "Done"
     ;;
    *)
 	echo "usage: $0 {create|drop|export|import}"
