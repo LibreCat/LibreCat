70fcb4a3a500f5c37151f1d9495ee1287e61c197
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2016-09-01 11:38:14 +0200

    layer aware catmandu commands

---
+++ lib/LibreCat/Cmd/convert.pm
@@ -0,0 +1,17 @@
+package LibreCat::Cmd::convert;
+
+use Catmandu::Sane;
+use parent qw(Catmandu::Cmd::convert);
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Cmd::convert - convert objects with catmandu
+
+=cut
+
---
+++ lib/LibreCat/Cmd/drop.pm
@@ -0,0 +1,17 @@
+package LibreCat::Cmd::drop;
+
+use Catmandu::Sane;
+use parent qw(Catmandu::Cmd::drop);
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Cmd::drop - drop a store or one of its bags with catmandu
+
+=cut
+
