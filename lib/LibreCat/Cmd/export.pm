9b5639ef1f0495ed5617617e1ee71eabb6e09966
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2016-09-01 14:50:19 +0000

    Adding documentation and warnings

--- lib/LibreCat/Cmd/export.pm
+++ lib/LibreCat/Cmd/export.pm
@@ -3,6 +3,22 @@ package LibreCat::Cmd::export;
 use Catmandu::Sane;
 use parent qw(Catmandu::Cmd::export);
 
+sub description {
+    return <<EOF
+WARNING - Low level command
+
+WARNING - These low level commands will skip all validation/business rules!
+
+Usage:
+
+librecat export <STORE> <OPTIONS> to <EXPORTER> <OPTIONS>
+
+librecat export search --bag publication to YAML
+
+Options:
+EOF
+}
+
 1;
 
 __END__
@@ -11,6 +27,6 @@ __END__
 
 =head1 NAME
 
-LibreCat::Cmd::export - export objects from a metadata store with catmandu
+LibreCat::Cmd::export - manage librecat metadata store - export objects
 
 =cut
