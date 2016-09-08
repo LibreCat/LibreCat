a81d24dd1272fc16a7956c9ad042f2162757737d
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2016-09-08 14:53:09 +0200

    layers support for authentication_admin.pl

--- bin/authentication_admin.pl
+++ bin/authentication_admin.pl
@@ -1,54 +1,60 @@
 #!/usr/bin/env perl
+
 =head1 NAME
 
 authentication_test.pl - Test your authentication settings
 
 =head1 SYNOPSIS
 
-	$ perl bin/authentication_test.pl mylogin -p
-	Password: *****
-	OK
+    $ perl bin/authentication_test.pl mylogin -p
+    Password: *****
+    OK
 
-	$ perl bin/authentication_test.pl
-	           --package=Authentication::Test
-	           --param foo=bar
-	           --param test=ok  mylogin -p
-	Password: *****
-	OK
+    $ perl bin/authentication_test.pl
+               --package=Authentication::Test
+               --param foo=bar
+               --param test=ok  mylogin -p
+    Password: *****
+    OK
 
 =head1 SEE ALSO
 
 L<log4perl.conf>
 
 =cut
-use lib qw(./lib);
+
+BEGIN {
+    use Catmandu::Sane;
+    use Path::Tiny;
+    use lib path(__FILE__)->parent->parent->child('lib')->stringify;
+    use LibreCat::Layers;
+
+    LibreCat::Layers->new->load;
+}
+
 use Catmandu;
 use Catmandu::Util;
 use LibreCat::User;
 use Getopt::Long;
 use Log::Log4perl;
-use Log::Any::Adapter;
 
-my $package  = Catmandu->config->{authentication}->{package};
-my $param    = Catmandu->config->{authentication}->{options} // {};
+my $package  = Catmandu->config->{authentication}{package};
+my $param    = Catmandu->config->{authentication}{options} // {};
 my $password;
 
-Log::Log4perl::init('log4perl.conf');
-Log::Any::Adapter->set('Log4perl');
-
-my $logger     = Log::Log4perl->get_logger('authentication_admin');
+my $logger = Log::Log4perl->get_logger('authentication_admin');
 
 GetOptions("package=s" => \$package, "param=s%" => \$param, "p" => \$password);
 
-my $user  = shift;
+my $user = shift;
 
 die "usage: $0 [--package=MODULE] [[--param=...]] login" unless defined $user;
 
 if ($password) {
-	print "Password: ";
-	system('/bin/stty', '-echo');
-	$password = <>; chop($password);
-	system('/bin/stty', 'echo');
+    print "Password: ";
+    system('/bin/stty', '-echo');
+    $password = <>; chop($password);
+    system('/bin/stty', 'echo');
 }
 else {
     die "need a password";
@@ -64,12 +70,12 @@ my $verify  = $auth->authenticate( { username => $user , password => $password }
 my $exporter = Catmandu->exporter('YAML');
 
 if ($verify) {
-	print "OK\n";
+    print "OK\n";
     $exporter->add($userobj);
     $exporter->commit;
-	exit(0);
+    exit(0);
 }
 else {
-	print "FAILED\n";
-	exit(2);
+    print "FAILED\n";
+    exit(2);
 }
