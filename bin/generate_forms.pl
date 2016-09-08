a0af76e6da532f1d21c8fda4f0a30383f961d0a1
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2016-09-08 13:26:58 +0200

    layers support for generate_forms

--- bin/generate_forms.pl
+++ bin/generate_forms.pl
@@ -1,11 +1,20 @@
 #!/usr/bin/env perl
 
-use FindBin qw($Bin);
+my $layers;
+
+BEGIN {
+    use Catmandu::Sane;
+    use Path::Tiny;
+    use lib path(__FILE__)->parent->parent->child('lib')->stringify;
+    use LibreCat::Layers;
+
+    LibreCat::Layers->new->load;
+};
+
 use Catmandu::Sane;
 use Catmandu;
 use Template;
 
-Catmandu->load(':up');
 my $conf = Catmandu->config;
 
 my $forms = $conf->{forms}->{publication_types};
@@ -15,8 +24,8 @@ my $tt = Template->new(
     START_TAG  => '{%',
     END_TAG    => '%}',
     ENCODING     => 'utf8',
-    INCLUDE_PATH => "$Bin/../views/backend/generator",
-    OUTPUT_PATH  => "$Bin/../views/backend/forms",
+    INCLUDE_PATH => Catmandu->root.'/views/backend/generator',
+    OUTPUT_PATH  => Catmandu->root.'/views/backend/forms',
 );
 
 foreach my $type ( keys %$forms ) {
@@ -24,10 +33,10 @@ foreach my $type ( keys %$forms ) {
     my $type_hash = $forms->{$type};
     $type_hash->{field_order} = $conf->{forms}->{field_order};
     if($type_hash->{fields}){
-    	print "Generating $type.tt\n";
-    	$tt->process( "master.tt", $type_hash, "$type.tt" ) || die $tt->error(), "\n";
-    	print "Generating expert/$type.tt\n";
-    	$tt->process( "master_expert.tt", $type_hash, "expert/$type.tt" ) || die $tt->error(), "\n";
+        print "Generating $type.tt\n";
+        $tt->process( "master.tt", $type_hash, "$type.tt" ) || die $tt->error(), "\n";
+        print "Generating expert/$type.tt\n";
+        $tt->process( "master_expert.tt", $type_hash, "expert/$type.tt" ) || die $tt->error(), "\n";
     }
 
 }
@@ -36,8 +45,8 @@ my $tta = Template->new(
     START_TAG  => '{%',
     END_TAG    => '%}',
     ENCODING     => 'utf8',
-    INCLUDE_PATH => "$Bin/../views/admin/generator",
-    OUTPUT_PATH  => "$Bin/../views/admin/forms",
+    INCLUDE_PATH => Catmandu->root.'/views/admin/generator',
+    OUTPUT_PATH  => Catmandu->root.'/views/admin/forms',
 );
 
 foreach my $item (keys %$other_items) {
