d6b3dc3b946f28bcac6ce27241b668e92c48bae9
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2016-09-09 16:19:51 +0200

    csrk header in ajax posts

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -1,34 +1,64 @@
 $(function () {
-	$('.mark').click(function(evt) {
-		evt.preventDefault();
-		var a = $(this);
-		var marked = a.data('marked');
-		if (marked == 0) {
-			$.post('/mark/'+a.data('id'), function(res) {
-				$('.total-marked').text(res.total);
-				a.data('marked', 1);
-				a.children('span').removeClass('fa-square-o');
-				a.children('span').addClass('fa-check-square-o');
-			}, 'json');
-		}
-		else {
-			$.post('/mark/'+a.data('id')+'?x-tunneled-method=DELETE', function(res) {
-				$('.total-marked').text(res.total);
-				a.data('marked', 0);
-				a.children('span').removeClass('fa-check-square-o');
-				a.children('span').addClass('fa-square-o');
-			}, 'json');
-			if(a.attr('id') && /clickme_(\d{1,})/i.test(a.attr('id'))){
-				var indexes = a.attr('id').match(/clickme_\d{1,}/i);
-				indexes[0] = indexes[0].replace(/clickme_/,"");
-				$('#fade_' + indexes[0]).fadeOut('slow', function() {});
-			}
-		}
-	});
+        $('.mark').click(function(evt) {
+                evt.preventDefault();
+                var a = $(this);
+                var marked = a.data('marked');
+                var csrfToken = $("meta[name='csrf_token']").attr("content");
+
+                if (marked == 0) {
+                        $.ajax({
+                            type: 'POST',
+                            url: '/mark/'+a.data('id'),
+                            headers: {
+                               "X-CSRF-Token": csrfToken
+                            },
+                            dataType: 'json',
+                            success: function(res) {
+                                $('.total-marked').text(res.total);
+                                a.data('marked', 1);
+                                a.children('span').removeClass('fa-square-o');
+                                a.children('span').addClass('fa-check-square-o');
+                            }
+                        });
+                }
+                else {
+                        $.ajax({
+                            type: 'POST',
+                            url: '/mark/'+a.data('id')+'?x-tunneled-method=DELETE',
+                            headers: {
+                               "X-CSRF-Token": csrfToken
+                            },
+                            dataType: 'json',
+                            success: function(res) {
+                                $('.total-marked').text(res.total);
+                                a.data('marked', 0);
+                                a.children('span').removeClass('fa-check-square-o');
+                                a.children('span').addClass('fa-square-o');
+                            }
+                        });
+                        if(a.attr('id') && /clickme_(\d{1,})/i.test(a.attr('id'))){
+                                var indexes = a.attr('id').match(/clickme_\d{1,}/i);
+                                indexes[0] = indexes[0].replace(/clickme_/,"");
+                                $('#fade_' + indexes[0]).fadeOut('slow', function() {});
+                        }
+                }
+        });
 });
 
-$( document ).ready(function() {
-    $.post('/marked_total', function(res) {
-    	$('.total-marked').text(res.total);
-    });
+$(document).ready(function() {
+    var totalMarked = $('.total-marked');
+    if (totalMarked.length) {
+        var csrfToken = $("meta[name='csrf_token']").attr("content");
+        $.ajax({
+            type: 'POST',
+            url: '/marked_total',
+            headers: {
+               "X-CSRF-Token": csrfToken
+            },
+            dataType: 'json',
+            success: function(res) {
+               totalMarked.text(res.total);
+            }
+        });
+    }
 });
