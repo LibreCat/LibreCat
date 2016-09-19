a1d039f370cff9a5f4eb10b1230861ec16bcb2a3
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2016-09-19 12:15:46 +0000

    Deleting unused config parameters. Generating forms in the layer (if available)

--- bin/generate_forms.pl
+++ bin/generate_forms.pl
@@ -15,43 +15,49 @@ use Catmandu::Sane;
 use Catmandu;
 use Template;
 
-my $conf = Catmandu->config;
+my $conf            = Catmandu->config;
+my $forms           = $conf->{forms}{publication_types};
+my $other_items     = $conf->{forms}{other_items};
+my $template_paths  = $layers->template_paths;
+my $output_path     = $template_paths->[0] . '/backend/forms';
 
-my $forms = $conf->{forms}{publication_types};
-my $other_items = $conf->{forms}{other_items};
+#-----------------
 
+print "[$output_path]\n";
 my $tt = Template->new(
     START_TAG  => '{%',
     END_TAG    => '%}',
     ENCODING     => 'utf8',
-    INCLUDE_PATH => [ map { "$_/backend/generator" } @{$layers->template_paths} ],
-    OUTPUT_PATH  => Catmandu->root.'/views/backend/forms',
+    INCLUDE_PATH => [ map { "$_/backend/generator" } @$template_paths ],
+    OUTPUT_PATH  => $output_path,
 );
 
 foreach my $type ( keys %$forms ) {
-
     my $type_hash = $forms->{$type};
     $type_hash->{field_order} = $conf->{forms}{field_order};
     if($type_hash->{fields}){
-        print "Generating $type.tt\n";
+        print "Generating $output_path/$type.tt\n";
         $tt->process( "master.tt", $type_hash, "$type.tt" ) || die $tt->error(), "\n";
-        print "Generating expert/$type.tt\n";
+        print "Generating $output_path/expert/$type.tt\n";
         $tt->process( "master_expert.tt", $type_hash, "expert/$type.tt" ) || die $tt->error(), "\n";
     }
-
 }
+
+#-----------------
+
+$output_path = $template_paths->[0] . '/admin/forms';
+
+print "[$output_path]\n";
 
 my $tta = Template->new(
     START_TAG  => '{%',
     END_TAG    => '%}',
     ENCODING     => 'utf8',
-    INCLUDE_PATH => Catmandu->root.'/views/admin/generator',
-    OUTPUT_PATH  => Catmandu->root.'/views/admin/forms',
+    INCLUDE_PATH => [ map { "$_/admin/generator" } @$template_paths ],
+    OUTPUT_PATH  => $output_path,
 );
 
 foreach my $item (keys %$other_items) {
-
-    print "Generating edit_$item page\n";
+    print "Generating $output_path/edit_$item page\n";
     $tta->process( "master_$item.tt", $other_items->{$item}, "edit_$item.tt" ) || die $tta->error(), "\n";
-
 }
