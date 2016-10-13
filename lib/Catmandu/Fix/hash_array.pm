e04df8f43abaa1b632c233f8df51bd59f1547fb9
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2016-10-13 09:45:39 +0000

    Making the hash_array() fix faster

--- lib/Catmandu/Fix/hash_array.pm
+++ lib/Catmandu/Fix/hash_array.pm
@@ -5,28 +5,34 @@ use Catmandu::Util qw(:array);
 use LibreCat::App::Helper;
 use Moo;
 
+use Data::Dumper;
+
+has 'array_fields' => (is => 'lazy');
+
+sub _build_array_fields {
+    my $self = $_[0];
+    my $arr  = h->config->{forms}->{array_field} // [];
+
+    my %lookup = map { $_ => 1 } @$arr;
+    \%lookup;
+}
+
 sub fix {
     my ($self, $data) = @_;
 
-    my $conf = h->config;
-    my $tmp_type;
-    if ($data->{type} =~ /^bi[A-Z]/) {
-        $tmp_type = "bithesis";
-    }
-    else {
-        $tmp_type = lc($data->{type});
-    }
-    my $fields = $conf->{forms}->{publication_types}->{$tmp_type}->{fields};
-
+    # Validating the publiction keys. If in the configuration file
+    # they are defined as arrays, forces them to be arrays.
     foreach my $key (keys %$data) {
-        my $ref         = ref $data->{$key};
-        my $array_field = $conf->{forms}->{array_field};
+        my $ref           = ref($data->{$key});
 
-        if ($ref ne "ARRAY" and array_includes($array_field, $key)) {
+        if ($ref eq "ARRAY" and ! $self->array_fields->{$key}) {
+            $data->{$key} = $data->{$key}->[0];
+        }
+        elsif ($ref ne "ARRAY" and $self->array_fields->{$key}) {
             $data->{$key} = [$data->{$key}];
         }
-        if ($ref eq "ARRAY" and !array_includes($array_field, $key)) {
-            $data->{$key} = $data->{$key}->[0];
+        else {
+            # ok
         }
     }
 
