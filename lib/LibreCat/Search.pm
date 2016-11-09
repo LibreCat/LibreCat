bb14a2713230ff4153d1dfcf67a13e9f49e3b3f9
Vitali Peil <vitali.peil@uni-bielefeld.de>
2016-11-09 11:19:18 +0100

    fixes GH #84

--- lib/LibreCat/Search.pm
+++ lib/LibreCat/Search.pm
@@ -4,6 +4,7 @@ use Catmandu::Sane;
 use Catmandu::Util qw(trim :is);
 use Catmandu;
 use Moo;
+use Hash::Merge::Simple qw(merge);
 use namespace::clean;
 
 has store => (
@@ -23,31 +24,23 @@ sub search {
     my $store = $self->store;
     my $bag = $store->bag($bag_name);
 
-    $bag->search(
+    my $hits = $bag->search(
         cql_query => $cql // '',
         sru_sortkeys => $self->_sru_sort($p->{sort}) // '',
         limit => $p->{limit} // Catmandu->config->{default_page_size},
         start => $p->{start} // 0,
-        facets => $p->{facets} // $self->_default_facets,
+        facets => merge($p->{facets}, $self->_default_facets),
     );
+
+    for (qw(next_page previous_page)) {
+        $hits->{$_} = $hits->$_;
+    }
+    $hits;
 }
 
 
 sub _default_facets {
-    return {
-        author      => {terms => {field => 'author.id',        size => 20,}},
-        editor      => {terms => {field => 'editor.id',        size => 20,}},
-        open_access => {terms => {field => 'file.open_access', size => 1}},
-        popular_science => {terms => {field => 'popular_science', size => 1}},
-        extern          => {terms => {field => 'extern',          size => 2}},
-        status          => {terms => {field => 'status',          size => 8}},
-        year            => {
-            terms => {field => 'year', size => 100, order => 'reverse_term'}
-        },
-        type => {terms => {field => 'type', size => 25}},
-        isi  => {terms => {field => 'isi',  size => 1}},
-        pmid => {terms => {field => 'pmid', size => 1}},
-    };
+    state $facets = Catmandu->config->{default_facets};
 }
 
 sub _sru_sort {
