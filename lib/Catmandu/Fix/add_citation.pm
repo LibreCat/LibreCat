LibreCat/LibreCat

commit 70a267f969a402393b816a8ce32c67adf2e7caa8

test skip citation option; fixes GH #78

config/schemas.yml
@@ -764,8 +764,6 @@ publication:
                 - type: string
                 - type: integer
       description: "Returned Record Ids"
-    citation:
-      type: object
 
   # A author|editor is required..can this be expressed in JSONSchema?
   required: ["_id","type","title","year","status"]

config/store.yml
@@ -1037,6 +1037,7 @@ search:
           defense_date: {type: date, index: no}
           locked: {type: byte}
           user_id: {type: string, index: no}
+          citation: {index: not_analyzed, type: object}
 
       researcher:
         include_in_all: false

lib/LibreCat/Cmd/publication.pm
@@ -177,7 +177,7 @@ sub _add {
                 $rec->{_id} //= $helper->new_record('publication');
                 $helper->store_record('publication', $rec, $skip_citation);
                 print "added $rec->{_id}\n";
-                return 0;
+                return 1;
             }
             else {
                 print STDERR join("\n",

commit 0e27f1db0388d93d84772824298fb6df8480e3ad

fixes GH #58 and more
- fix bug introduced by 70a267f969a402393b816a8ce32c67adf2e7caa8
- app is graceful to citation settings
- displays more fields on frontdoor

config/citation.yml
@@ -2,7 +2,7 @@ _prefix:
   citation:
 
 # engine for citation generation
-# possible values are 'template' or 'csl' or 'none'
+# possible values are 'csl' or 'none'
 engine: none
 
 template:
@@ -12,9 +12,8 @@ csl:
   url: 'http://localhost:8085'
   default_style: apa
   styles:
-    - modern-language-association
-    - chicago-fullnote-bibliography
-    - american-medical-association
-    - angewandte-chemie
     - apa
+    - american-medical-association
+    - chicago-fullnote-bibliography
     - ieee
+    - modern-language-association

config/locale.de.yml
@@ -237,7 +237,18 @@ facets:
     "publcount.desc": "Anzahl der Publikationen absteigend"
     "fullname.asc": "Alphabetisch aufsteigend"
     "fullname.desc": "Alphabetisch absteigend"
+citation:
+  apa: APA
+  american-medical-association: AMA
+  chicago-fullnote-bibliography: Chicago
+  ieee: IEEE
+  modern-language-association: MLA
 frontdoor:
+  publication: Zeitschriftentitel
+  volume: Band
+  Issue: Ausgabe
+  page: "Seite(n)"
+  article_number: Artikelnr.
   preview: "Vorschau"
   crumb_back: "Zur&uuml;ck"
   no_record: "Dieser Eintrag exisitert nicht oder wurde gel&ouml;scht"

config/locale.en.yml
@@ -237,7 +237,18 @@ facets:
     "publcount.desc": "Number of publications descending"
     "fullname.asc": "Alphabetically ascending"
     "fullname.desc": "Alphabetically descending"
+citation:
+  apa: APA
+  american-medical-association: AMA
+  chicago-fullnote-bibliography: Chicago
+  ieee: IEEE
+  modern-language-association: MLA
 frontdoor:
+  publication: Journal Title
+  volume: Volume
+  Issue: Issue
+  page: Page
+  article_number: Article no.
   preview: "Preview"
   crumb_back: "Back"
   no_record: "This record does not exist or was deleted"

config/schemas.yml
@@ -764,6 +764,8 @@ publication:
                 - type: string
                 - type: integer
       description: "Returned Record Ids"
+    citation:
+      type: object
 
   # A author|editor is required..can this be expressed in JSONSchema?
   required: ["_id","type","title","year","status"]

lib/Catmandu/Fix/add_citation.pm
@@ -9,8 +9,8 @@ use Moo;
 sub fix {
     my ($self, $data) = @_;
 
-    unless (h->config->{citation}->{engine} eq 'none') {
-        state $citation_engine = LibreCat::Citation->new(all => 1);
+    if (h->config->{citation}->{engine} eq 'csl') {
+        my $citation_engine = LibreCat::Citation->new(all => 1);
 
         my $d = clone $data;
         $data->{citation} = $citation_engine->create($d);

lib/LibreCat/App/Helper.pm
@@ -523,11 +523,9 @@ sub store_record {
     # memoize fixes
     state $fixes = {};
     my $fix = $fixes->{$bag} //= $self->create_fixer("update_$bag.fix");
-    $self->log->debug("fixing using update_$bag.fix");
-
     $fix->fix($rec);
 
-    my $cite_fix = Catmandu::Fix->new(fixes => ["add_citation()"]);
+    state $cite_fix = Catmandu::Fix->new(fixes => ["add_citation()"]);
     if ($bag eq 'publication' && !$skip_citation) {
         $cite_fix->fix($rec);
     }

views/hits.tt
@@ -81,9 +81,6 @@
 [% END %]
     <em>[% IF entry.year %][% entry.year %][% ELSE %][n.d.][% END %] | [% lf.forms.item(entry.type).label %] | [% h.loc("hits.id") %]: <span class="pubid">[% entry._id %]</span> </em>
     <div class="citation-block-div">
-    [% IF style == 'default' %]
-	<a href="[% h.host %]/[% IF backend %]librecat/record/edit[% ELSE %][% bag %][% END %]/[% entry._id %]"[% IF request.path.match("/embed") %] target="_blank"[% END %]><strong>[% entry.title | html %]</strong></a><br>
-    [% END %]
     [%- IF backend AND ((!entry.locked AND !entry.type.match("^bi")) OR session.role == "super_admin") %]
       <a href="/librecat/record/edit/[% entry._id %]" onclick="window.top.location='/librecat/record/edit/[% entry._id %]'" class="citation-block-link"></a>
     [%- ELSIF backend AND entry.status != "public" %]
@@ -91,7 +88,8 @@
     [%- END %]
     [%- IF h.config.citation.engine == 'csl' AND entry.citation.$style %]
       [% entry.citation.$style %]
-    [%- ELSE %][% PROCESS citation.tt entry=entry %]
+    [%- ELSE %]
+    [% PROCESS citation.tt entry=entry %]
     [%- END %]
     </div>
     [% INCLUDE links.tt %]

views/publication/record.tt
@@ -60,10 +60,10 @@
   [%- END %]
 [%- END %]
 [%- IF h.config.citation.engine == 'csl' AND citation %][% display_citation = 1 %][% END %]
-[%- qp = request.params.hash %]
-[%- style = style || qp.style || 'default' %]
+[%- style = h.config.citation.csl.default_style %]
 
-<div class="row" itemscope itemtype="http://schema.org/[% schema.type %]" itemid="[% h.host %]/publication/[% _id %]"><!-- outer row-->
+[% mypath = request.path.match('/publication') ? 'publication' : 'data' %]
+<div class="row" itemscope itemtype="http://schema.org/[% schema.type %]" itemid="[% h.host %]/[% mypath %]/[% _id %]"><!-- outer row-->
   <div class="col-md-11 col-sm-12"><!-- outer col -->
     <div class="page-header" id="banner">
       [%- UNLESS _id %]

views/publication/tab_details.tt
@@ -106,14 +106,20 @@
     </div>
   </div>
 [%- END %]
+
   <div class="row">
     <div class="col-lg-2 col-md-3 text-muted">[% lf.forms.${type}.field.year.label || h.loc("forms.${type}.field.year_extern.label") %]</div>
     <div class="col-lg-10 col-md-9"><span itemprop="copyrightYear"><a href="/[% bag %]?q=year=[% year %]">[% year %]</a></span></div>
   </div>
-[%- IF !display_citation %]
   [%- FOREACH item IN ['publication', 'volume', 'issue', 'article_number', 'page'] %]
+    [%- IF $item %]
+      <div class="row">
+        <div class="col-lg-2 col-md-3 text-muted">[% h.loc("frontdoor.${item}") %]</div>
+        <div class="col-lg-10 col-md-9"><span>[% $item %]</span></div>
+      </div>
+    [%- END %]
   [%- END %]
-[%- END %]
+
 [%- IF data_reuse_license %]
   <div class="row">
     <div class="col-lg-2 col-md-3 text-muted">[% h.loc("forms.${type}.field.license.data_reuse_license.label") %]</div>
@@ -202,32 +208,23 @@
     </div>
   </div>
   [%- IF display_citation %]
-  <!-- Citations -->
-  <div id="citethis">
-  <h3 id="cite">[% h.loc("frontdoor.cite_this") %]</h3>
-  <ul class="nav nav-tabs">
-    <li class="active"><a href="#ama" data-toggle="tab">AMA</a></li>
-    <li><a href="#apa" data-toggle="tab">APA</a></li>
-    <li><a href="#frontiers" data-toggle="tab">Frontiers</a></li>
-    <li><a href="#harvard1" data-toggle="tab">Harvard</a></li>
-    <li><a href="#ieee" data-toggle="tab">IEEE</a></li>
-    <li><a href="#lncs" data-toggle="tab">LNCS</a></li>
-    <li><a href="#mla" data-toggle="tab">MLA</a></li>
-    <li><a href="#bibtex" data-toggle="tab">BibTeX</a></li>
-    <li><a href="#ris" data-toggle="tab">RIS</a></li>
-  </ul>
-  <div class="tab-content">
-    <div id="ama" class="tab-pane active cmark">[% citation.ama %]</div>
-    <div id="apa" class="tab-pane cmark">[% citation.apa %]</div>
-    <div id="frontiers" class="tab-pane cmark">[% citation.frontiers %]</div>
-    <div id="harvard1" class="tab-pane cmark">[% citation.harvard1 %]</div>
-    <div id="ieee" class="tab-pane cmark">[% citation.ieee %]</div>
-    <div id="lncs" class="tab-pane cmark">[% citation.lncs %]</div>
-    <div id="mla" class="tab-pane cmark">[% citation.mla %]</div>
-    <div id="bibtex" class="tab-pane"></div>
-    <div id="ris" class="tab-pane"></div>
-  </div>
-  </div><!-- citethis -->
+    <div id="citethis">
+      <h3 id="cite">[% h.loc("frontdoor.cite_this") %]</h3>
+      <ul class="nav nav-tabs">
+        [%- FOREACH style IN h.config.citation.csl.styles %]
+          <li [% IF loop.first %]class="active"[% END %]><a href="#[% style %]" data-toggle="tab">[% h.loc("citation.${style}")%]</a></li>
+        [% END %]
+        <li><a href="#bibtex" data-toggle="tab">BibTeX</a></li>
+        <li><a href="#ris" data-toggle="tab">RIS</a></li>
+      </ul>
+      <div class="tab-content">
+        [%- FOREACH style IN h.config.citation.csl.styles %]
+          <div id="[% style %]" class="tab-pane[% IF loop.first %] active[% END %] cmark">[% citation.$style %]</div>
+        [% END %]
+        <div id="bibtex" class="tab-pane"></div>
+        <div id="ris" class="tab-pane"></div>
+      </div>
+    </div>
   [%- END %]
 </div>
 