1de6f89e58fb430e86d67ca56cbd9cca1cd341be
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2016-12-01 12:41:12 +0000

    Providing examples in the startup scripts to reroute the TMP directory

--- boot.sh
+++ boot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## Optionally provide a directory where to store the Dancer
+## temporary files
+# export TMPDIR=/data2/tmp
+
 if [ "$1" == "starman" ]; then
     carton exec 'plackup -R lib -s Starman --port 5001  bin/app.pl'
 else
