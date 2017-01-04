e2564e308e1101ffb664fc2200b43767b45cf2a5
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2017-01-04 12:44:30 +0000

    Adding audit worker

---
+++ lib/LibreCat/Worker/Audit.pm
@@ -0,0 +1,36 @@
+package LibreCat::Worker::Audit;
+
+use Catmandu::Sane;
+use Catmandu;
+use Moo;
+use namespace::clean;
+
+with 'LibreCat::Worker';
+
+sub work {
+    my ($self, $opts) = @_;
+
+    my $store = Catmandu->store('backup')->bag('audit');
+
+    unless ($store) {
+        $self->log->error("failed to find 'backup.audit' store");
+        return;
+    }
+
+    $self->log->debugf("audit message: %s", $opts);
+
+    my $rec = {
+        pid     => $opts->{id} ,
+        bag     => $opts->{bag} ,
+        process => $opts->{process} ,
+        action  => $opts->{action} ,
+        message => $opts->{message} ,
+        time    => time
+    };
+
+    $store->add($rec);
+
+    $store->commit();
+}
+
+1;
