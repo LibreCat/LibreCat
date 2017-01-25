be43e052dfbaacc4433bc47ab505a7396c7e16e9
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2017-01-25 12:41:47 +0000

    Provide example scripts for executing cron jobs

---
+++ bin/exec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Bash script used for executing crontab command
+#
+# E.g. bin/exec.sh bin/librecat publication export
+
+INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${INSTALL_DIR}/..
+
+if [ ! -r env.sh ]; then
+    echo "error - need a ${INSTALL_DIR}/env.sh!"
+    exit 2
+else
+    source env.sh
+fi
+
+carton exec "$@"
---
+++ env.sh
@@ -0,0 +1,4 @@
+# Add all environment variables needed for running crontab scripts
+LIBRECAT_HOME=/opt/librecat
+PERL_BINS=${HOME}/.plenv/shims
+PATH=${PERL_BINS}:${PATH}
