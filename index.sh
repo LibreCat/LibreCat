e2a38b4e75c7576d38c0c1d6607e603898f979fe
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2017-03-21 12:16:38 +0000

    Adding tools to drop and reindex all elasticsearch indexes

--- index.sh
+++ index.sh
@@ -7,59 +7,75 @@ case "${CMD}" in
     create)
         echo "Creating index..."
         echo "researcher..."
-        carton exec bin/librecat user add devel/researcher.yml
+        carton exec "bin/librecat user add devel/researcher.yml"
         echo "publication..."
-        carton exec bin/librecat publication add devel/publications.yml
+        carton exec "bin/librecat publication add devel/publications.yml"
         echo "department..."
-        carton exec bin/librecat department tree devel/department-tree.yml
+        carton exec "bin/librecat department tree devel/department-tree.yml"
         echo "project..."
-        carton exec bin/librecat project add devel/project.yml
+        carton exec "bin/librecat project add devel/project.yml"
         echo "Generating tree"
-        carton exec bin/librecat generate departments
+        carton exec "bin/librecat generate departments"
         echo "Done"
         ;;
     drop)
         echo "Dropping index.."
         echo "researcher..."
-        carton exec bin/librecat delete search --bag researcher
+        carton exec "bin/librecat delete search --bag researcher"
         echo "publication..."
-        carton exec bin/librecat delete search --bag publication
+        carton exec "bin/librecat delete search --bag publication"
         echo "department..."
-        carton exec bin/librecat delete search --bag department
+        carton exec "bin/librecat delete search --bag department"
         echo "research_group..."
-        carton exec bin/librecat delete search --bag research_group
+        carton exec "bin/librecat delete search --bag research_group"
         echo "project..."
-        carton exec bin/librecat delete search --bag project
+        carton exec "bin/librecat delete search --bag project"
         echo "Done"
         ;;
     drop_backup)
         echo "Dropping backup.."
         echo "ids..."
-        carton exec bin/librecat delete default --bag data
+        carton exec "bin/librecat delete default --bag data"
         echo "researcher..."
-        carton exec bin/librecat delete backup  --bag researcher
+        carton exec "bin/librecat delete backup  --bag researcher"
         echo "publication..."
-        carton exec bin/librecat delete backup  --bag publication
+        carton exec "bin/librecat delete backup  --bag publication"
         echo "department..."
-        carton exec bin/librecat delete backup  --bag department
+        carton exec "bin/librecat delete backup  --bag department"
         echo "project..."
-        carton exec bin/librecat delete backup  --bag project
+        carton exec "bin/librecat delete backup  --bag project"
         echo "research_group..."
-        carton exec bin/librecat delete backup  --bag research_group
+        carton exec "bin/librecat delete backup  --bag research_group"
         echo "Done"
         ;;
     drop_version)
         echo "Dropping backup.."
         echo "researcher..."
-        carton exec bin/librecat delete backup  --bag researcher_version
+        carton exec "bin/librecat delete backup  --bag researcher_version"
         echo "publication..."
-        carton exec bin/librecat delete backup  --bag publication_version
+        carton exec "bin/librecat delete backup  --bag publication_version"
         echo "department..."
-        carton exec bin/librecat delete backup  --bag department_version
+        carton exec "bin/librecat delete backup  --bag department_version"
         echo "project..."
-        carton exec bin/librecat delete backup  --bag project_version
+        carton exec "bin/librecat delete backup  --bag project_version"
         echo "research_group..."
-        carton exec bin/librecat delete backup  --bag research_group_version
+        carton exec "bin/librecat delete backup  --bag research_group_version"
+        echo "Done"
+        ;;
+    reindex)
+        echo "Dropping the search"
+        carton exec bin/librecat drop search
+        echo "Reindex:"
+        echo "researcher"
+        carton exec "bin/librecat copy -v backup --bag researcher to search --bag researcher"
+        echo "publication"
+        carton exec "bin/librecat copy -v backup --bag publication to search --bag publication"
+        echo "department"
+        carton exec "bin/librecat copy -v backup --bag department to search --bag department"
+        echo "project"
+        carton exec "bin/librecat copy -v backup --bag project to search --bag project"
+        echo "research_group."
+        carton exec "bin/librecat copy -v backup --bag research_group. to search --bag research_group"
         echo "Done"
         ;;
     export)
@@ -68,15 +84,15 @@ case "${CMD}" in
         mkdir -p ${TMPDIR}
 
         echo "researcher..."
-        carton exec bin/librecat user list > ${TMPDIR}/researcher.yml
+        carton exec "bin/librecat user list" > ${TMPDIR}/researcher.yml
         echo "publication..."
-        carton exec bin/librecat publication list > ${TMPDIR}/publications.yml
+        carton exec "bin/librecat publication list" > ${TMPDIR}/publications.yml
         echo "department..."
-        carton exec bin/librecat department list > ${TMPDIR}/department.yml
+        carton exec "bin/librecat department list" > ${TMPDIR}/department.yml
         echo "project..."
-        carton exec bin/librecat project list > ${TMPDIR}/project.yml
+        carton exec "bin/librecat project list" > ${TMPDIR}/project.yml
         echo "research_group..."
-        carton exec bin/librecat research_group list > ${TMPDIR}/research_group.yml
+        carton exec "bin/librecat research_group list" > ${TMPDIR}/research_group.yml
 
         cd ${TMPDIR}
 
@@ -109,15 +125,15 @@ case "${CMD}" in
         cd -
 
         echo "researcher..."
-        carton exec bin/librecat user add ${TMPDIR}/researcher.yml
+        carton exec "bin/librecat user add" ${TMPDIR}/researcher.yml
         echo "publications..."
-        carton exec bin/librecat publication add ${TMPDIR}/publications.yml
+        carton exec "bin/librecat publication add" ${TMPDIR}/publications.yml
         echo "department..."
-        carton exec bin/librecat department add ${TMPDIR}/department.yml
+        carton exec "bin/librecat department add" ${TMPDIR}/department.yml
         echo "project..."
-        carton exec bin/librecat project add ${TMPDIR}/project.yml
+        carton exec "bin/librecat project add" ${TMPDIR}/project.yml
         echo "research_group..."
-        carton exec bin/librecat research_group add ${TMPDIR}/research_group.yml
+        carton exec "bin/librecat research_group add" ${TMPDIR}/research_group.yml
 
         rm -rf ${TMPDIR}
 
