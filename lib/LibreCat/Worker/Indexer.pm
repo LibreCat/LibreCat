95c375d943036b319db7bb34db1f27daacf28ed1
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2017-04-27 09:41:11 +0000

    Fixing bug in single record reindexing

--- lib/LibreCat/Worker/Indexer.pm
+++ lib/LibreCat/Worker/Indexer.pm
@@ -24,7 +24,7 @@ sub index_record {
     my ($self, $opts, $job) = @_;
 
     my $bag = $opts->{bag};
-    my $id  = $opts->{bag};
+    my $id  = $opts->{id};
 
     my $source = Catmandu->store('backup')->bag($bag);
     my $target = Catmandu->store('search')->bag($bag);
