562b62c9c71713c4a8831b72f456425337072eb9
Matthias Feldotto <feldi88@users.noreply.github.com>
2018-01-02 11:58:52 +0100

    Revert "Fix typo in error message"
    This reverts commit 33af9e2996ae06df3d47442d03c7de75db510361.

--- lib/LibreCat/Cmd/file_store.pm
+++ lib/LibreCat/Cmd/file_store.pm
@@ -457,7 +457,7 @@ sub _move_files {
         else { # Child
             $pipe->writer();
             $source_files->stream($pipe,$file)
-                || croak "failed to stream $name : $!";
+                || croak "faied to stream $name : $!";
             exit(0);
         }
 
