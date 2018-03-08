d04c8cce26c9a779898aa077983a01214ea402dc
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2018-03-08 08:27:59 +0000

    Moving ocrid processing to person fix

--- lib/Catmandu/Fix/person.pm
+++ lib/Catmandu/Fix/person.pm
@@ -4,26 +4,45 @@ package Catmandu::Fix::person;
 
 =head1 NAME
 
-Catmandu::Fix::person - generate a full_name if not exists
+Catmandu::Fix::person - generate a full_name and orcid if not exists
 
 =cut
 
 use Catmandu::Sane;
 use Catmandu::Util qw(:is);
+use LibreCat::App::Helper;
 use Moo;
+
+has 'helper' => (is => 'lazy');
+
+sub _build_helper {
+    LibreCat::App::Helper::Helpers->new;
+}
 
 sub fix {
     my ($self, $data) = @_;
     my @types = qw(author editor translator supervisor corporate_editor applicant);
 
+    my $users = $self->helper->main_user;
+
     foreach my $author_type (@types) {
         next unless $data->{$author_type};
         foreach my $au (@{$data->{$author_type}}) {
+            # Update the full_name
             if (is_string($au->{full_name})) {
                 # ok
             }
             elsif (is_string($au->{first_name}) && is_string($au->{last_name})) {
                 $au->{full_name} = $au->{last_name} . ", " . $au->{first_name};
+            }
+
+            # Update the orcid
+            if ($au->{orcid}) {
+                # ok
+            }
+            elsif (is_string($au->{id})) {
+                my $user = $users->get($au->{id});
+                $au->{orcid} = $user->{orcid} if ($user && $user->{orcid});
             }
         }
     }
