1f90e4e952a7e37d858f6dc3c982b08a8d0f43c5
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2018-03-08 07:20:43 +0000

    Fixing validation on required first_name and last_name

--- lib/Catmandu/Fix/person.pm
+++ lib/Catmandu/Fix/person.pm
@@ -1,35 +1,33 @@
 package Catmandu::Fix::person;
 
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::person - generate a full_name if not exists
+
+=cut
+
 use Catmandu::Sane;
+use Catmandu::Util qw(:is);
 use Moo;
 
 sub fix {
     my ($self, $data) = @_;
-    my @p = qw(author editor translator supervisor honoree);
+    my @types = qw(author editor translator supervisor corporate_editor applicant);
 
-    foreach my $pers (@p) {
-        next unless $data->{$pers};
-
-        @{$data->{$pers}} = grep defined, @{$data->{$pers}};
-        my $splice_me;
-        my $i = 0;
-
-        foreach (@{$data->{$pers}}) {
-            if ($_->{first_name} and $_->{last_name}) {
-                $_->{full_name} = $_->{last_name} . ", " . $_->{first_name};
-                $i++;
+    foreach my $author_type (@types) {
+        next unless $data->{$author_type};
+        foreach my $au (@{$data->{$author_type}}) {
+            if (is_string($au->{full_name})) {
+                # ok
             }
-            else {
-                push @$splice_me, $i;
-            }
-        }
-
-        if ($splice_me) {
-            foreach (@$splice_me) {
-                splice @{$data->{$pers}}, $_, 1;
+            elsif (is_string($au->{first_name}) && is_string($au->{last_name})) {
+                $au->{full_name} = $au->{last_name} . ", " . $au->{first_name};
             }
         }
     }
+
     return $data;
 }
 
