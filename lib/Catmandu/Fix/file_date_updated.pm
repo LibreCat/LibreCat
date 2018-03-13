df950ae9667b1c0caf57fd89aa9d72681c35b9b5
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2018-03-13 10:44:24 +0000

    Adding tests

--- lib/Catmandu/Fix/file_date_updated.pm
+++ lib/Catmandu/Fix/file_date_updated.pm
@@ -1,33 +1,10 @@
 package Catmandu::Fix::file_date_updated;
 
-use Catmandu::Sane;
-use POSIX qw(floor);
-use Moo;
-
-sub fix {
-    my ($self, $data) = @_;
-
-    my @dates = ();
-
-    for my $file (@{ $data->{file} }) {
-	    my $date_updated = $file->{date_updated};
-	    push @dates , $date_updated;
-    }
-
-    $data->{file_date_updated} = [ sort @dates ]->[-1] if @dates;
-
-    $data;
-}
-
-1;
-
-__END__
-
 =pod
 
 =head1 NAME
 
-file_date_updated - calculate the latest update date of record files
+Catmandu::Fix::file_date_updated - calculate the latest update date of record files
 
 =head1 SYNOPSIS
 
@@ -46,3 +23,26 @@ file_date_updated - calculate the latest update date of record files
     $ bin/librecat publication list "file_date_updated > 2017-01-01T00:00:00Z"
 
 =cut
+
+use Catmandu::Sane;
+use POSIX qw(floor);
+use Moo;
+
+sub fix {
+    my ($self, $data) = @_;
+
+    return $data unless $data->{file};
+    
+    my @dates = ();
+
+    for my $file (@{ $data->{file} }) {
+	    my $date_updated = $file->{date_updated};
+	    push @dates , $date_updated;
+    }
+
+    $data->{file_date_updated} = [ sort @dates ]->[-1] if @dates;
+
+    $data;
+}
+
+1;
--- t/Catmandu/Fix/file_date_updated.t
+++ t/Catmandu/Fix/file_date_updated.t
@@ -13,4 +13,20 @@ BEGIN {
 
 require_ok $pkg;
 
+is_deeply $pkg->new()->fix({ foo => 'bar'}), { foo => 'bar' };
+
+is_deeply $pkg->new()->fix({
+                file => [
+                    { date_updated => '1970-01-01T00:00:00Z' } ,
+                    { date_updated => '2050-01-01T00:00:00Z' } ,
+                ]
+            }),
+            {
+                file => [
+                    { date_updated => '1970-01-01T00:00:00Z' } ,
+                    { date_updated => '2050-01-01T00:00:00Z' } ,
+                ],
+                file_date_updated => '2050-01-01T00:00:00Z'
+            };
+
 done_testing;
