9f931cade18f24cd8bb09b18b576a21866c2d1e3
Vitali Peil <vitali.peil@uni-bielefeld.de>
2018-03-14 10:38:46 +0100

    add roles LibreCat::Model and LibreCat::Logger

---
+++ lib/LibreCat/Model.pm
@@ -0,0 +1,70 @@
+package LibreCat::Model;
+
+use Catmandu::Sane;
+use Moo::Role;
+use Catmandu::Util qw(require_package);
+use namespace::clean;
+
+with 'LibreCat::Logger';
+
+has bag        => (is => 'ro', required => 1);
+has search_bag => (is => 'ro', required => 1);
+
+has validator => (is => 'lazy');
+
+sub _build_validator {
+    my ($self) = @_;
+
+    require_package(ucfirst($self->bag), 'LibreCat::Validator')->new;
+}
+
+sub _index {
+    my ($self, $rec) = @_;
+
+    $self->search_bag->add($rec);
+    $self->search_bag->commit;
+
+    # $rec;
+}
+
+sub _purge {
+    my ($self, $id) = @_;
+
+    $self->bag->delete($id);
+    $self->bag->commit;
+
+    $self->search_bag->delete($id);
+    $self->search_bag->commit;
+
+    return +{};
+}
+
+sub _validate {
+    my ($self, $rec) = @_;
+
+    my $can_store = 1;
+
+    my $validator = $self->validator;
+    my $validator_pkg = ref $validator;
+
+    my @white_list = $validator->white_list;
+
+    $self->log->fatal("no white_list found for $validator_pkg ??!")
+        unless @white_list;
+
+    for my $key (keys %$rec) {
+        unless (grep(/^$key$/, @white_list)) {
+            $self->log->debug("deleting invalid key: $key");
+            delete $rec->{$key};
+        }
+    }
+
+    unless ($validator->is_valid($rec)) {
+        $can_store = 0;
+        $opts{validation_error}->($validator, $rec)
+            if $opts{validation_error}
+            && ref($opts{validation_error}) eq 'CODE';
+    }
+}
+
+1;
