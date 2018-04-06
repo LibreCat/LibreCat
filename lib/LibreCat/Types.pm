e3f9e7af7498983d8680e9c38f8877bde0cb6657
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2018-04-06 09:51:12 +0200

    LibreCat::Types pod

--- lib/LibreCat/Types.pm
+++ lib/LibreCat/Types.pm
@@ -10,3 +10,31 @@ use Type::Library
 declare Pairs, as CycleTuple[Str, Any], where { scalar(@$_) % 2 == 0 };
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Types - LibreCat custom types library
+
+=head1 SYNOPSIS
+
+    
+
+=head1 TYPES
+
+    use LibreCat::Types qw(+Pairs);
+
+=head2 Pairs
+
+An even sized ArrayRef of Str => Any pairs.
+
+    [foo => Foo->new, bar => 'bar']
+
+=head1 SEE ALSO
+
+L<Type::Library>
+
+=cut
