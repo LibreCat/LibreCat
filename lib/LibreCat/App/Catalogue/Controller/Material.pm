0d431e4cd1acd8fd230a026f5ee5758465cd411b
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2018-05-02 13:58:07 +0000

    Fixing #467

--- lib/LibreCat/App/Catalogue/Controller/Material.pm
+++ lib/LibreCat/App/Catalogue/Controller/Material.pm
@@ -7,134 +7,108 @@ use Exporter qw/import/;
 
 our @EXPORT = qw/update_related_material/;
 
-my $relations_link   = h->config->{lists}->{relations_link};
-my $relations_record = h->config->{lists}->{relations_record};
-my $rd_relation      = h->config->{lists}->{relations_rd};
-
 sub update_related_material {
-    my $pub                   = shift;
-    my $related_material_link = $pub->{related_material}->{'link'}
-        if $pub->{related_material}->{'link'};
-    my $related_material_record;
-    @$related_material_record = grep %$_,
-        @{$pub->{related_material}->{record}}
-        if $pub->{related_material}->{record};
+    my $pub = shift;
 
-    # get old related material (to be able to remove deleted relations)
-    my $hit                         = h->main_publication->get($pub->{_id});
-    my $old_related_material_record = $hit->{related_material}->{record}
-        if $hit->{related_material} and $hit->{related_material}->{record};
+    return unless $pub->{related_material};
 
-    foreach my $rm (@$related_material_record) {
+    #--- Create a inverse relation name database ---
+    my $relations_record = h->config->{lists}->{relations_record} // [];
+    my $rd_relation      = h->config->{lists}->{relations_rd} // [];
 
-        # set relation in other record
-        if ($rm->{id}) {
-            my $opposite = h->main_publication->get($rm->{id});
+    my $lookup_relation = {};
 
-            push(@$relations_record, @$rd_relation);
+    for (@$relations_record,@$rd_relation) {
+        my $relation = $_->{relation};
+        my $opposite = $_->{opposite};
+        $lookup_relation->{$relation} = $opposite;
+    }
+    #---
 
-            my ($ref)
-                = grep {$_->{relation} eq $rm->{relation}} @$relations_record;
-            my $op_relation = $ref->{opposite};
+    # First delete the relations to this record from previous record (in order to
+    # remove deleted relations from targetted records)
+    my $hit = h->main_publication->get($pub->{_id});
 
-            if (   !$opposite->{related_material}
-                or !$opposite->{related_material}->{record})
-            {
-                push @{$opposite->{related_material}->{record}},
-                    {
+    my $previous_related_material_record = $hit->{related_material}->{record} // [];
+
+    foreach my $rm (@$previous_related_material_record) {
+        next unless $rm->{id};
+
+        my $target = h->main_publication->get($rm->{id});
+
+        next unless $target;
+
+        my $target_related_material_record = $target->{related_material}->{record} // [];
+
+        my @new_relations;
+
+        for (@$target_related_material_record) {
+            if ($_->{id} eq $pub->{_id}) {
+                # remove
+            }
+            else {
+                # keep
+                push @new_relations , $_;
+            }
+        }
+
+        $target->{related_material}->{record} = \@new_relations;
+
+        my $saved = h->main_publication->add($target);
+        h->main_publication->commit;
+        h->publication->add($saved);
+        h->publication->commit;
+    }
+
+    # (Re)create the relations to other records as reverse links
+    my $current_related_material_record = $pub->{related_material}->{record} // [];
+
+    foreach my $rm (@$current_related_material_record) {
+        next unless $rm->{id};
+
+        my $target = h->main_publication->get($rm->{id});
+
+        next unless $target;
+
+        # Find the name of the reverse link
+        my $inverse_relation = $lookup_relation->{ $rm->{relation} };
+
+        unless ($inverse_relation) {
+            h->log->error("found not inverse relation for `" . $rm->{relation} . "`");
+            $inverse_relation = 'other';
+        }
+        
+        my $target_related_material_record = $target->{related_material}->{record} // [];
+
+        my @new_relations;
+
+        my $found = 0;
+
+        for (@$target_related_material_record) {
+            if ($_->{id} eq $pub->{_id}) {
+                $found = 1;
+                $_->{status}   = $pub->{status};
+                $_->{relation} = $inverse_relation;
+            }
+            push @new_relations , $_;
+        }
+
+        if (!$found) {
+            push @new_relations, {
                     id       => $pub->{_id},
                     status   => $pub->{status},
-                    relation => $op_relation
-                    };
-            }
-            elsif ( $opposite->{related_material}
-                and $opposite->{related_material}->{record})
-            {
-                my ($ref) = grep {
-                            $_->{relation} eq $op_relation
-                        and $_->{id} eq $pub->{_id}
-                } @{$opposite->{related_material}->{record}};
-                if (!$ref) {
-                    push @{$opposite->{related_material}->{record}},
-                        {
-                        id       => $pub->{_id},
-                        status   => $pub->{status},
-                        relation => $op_relation
-                        };
-                }
-                else {
-                    $ref->{status} = $pub->{status};
-                }
-            }
-            my $saved = h->main_publication->add($opposite);
-            h->main_publication->commit;
-            h->publication->add($saved);
-            h->publication->commit;
-
-            $rm->{status} = $opposite->{status};
-        }
-    }
-    if ($old_related_material_record) {
-
-        #@A @$old_related_material_record = 1..9;
-        #@B @$related_material_record = (2, 4, 6, 8);
-        #my @dense = grep %$_, @$related_material_record;
-        my %B = map {$_->{id} => 1} @$related_material_record;
-        my @recs = grep {not $B{$_->{id}}} @$old_related_material_record;
-
-        #$recs = [$recs] if ref $recs ne "ARRAY";
-        my $q;
-        my $query = "";
-        if (@recs) {
-            foreach my $id (@recs) {
-                $query .= " OR id=" . $id->{id};
-            }
-            $query =~ s/^ OR //g;
-            $query = "(" . $query . ")";
-
-            push @{$q->{cql_quey}}, $query;
-
-            $q->{limit} = 1000;
-
-            my $hits = LibreCat->searcher->search('publication', $q);
-            my $return_hit;
-
-            if ($hits->{total}) {
-                $hits->each(
-                    sub {
-                        my $rec = $_[0];
-                        $return_hit = $rec;
-
-                        if (    $rec->{related_material}
-                            and $rec->{related_material}->{record})
-                        {
-                            foreach my $rel (
-                                @{$rec->{related_material}->{record}})
-                            {
-                                if ($rel->{id} eq $pub->{_id}) {
-                                    $rel = {};
-                                }
-                            }
-                            @{$rec->{related_material}->{record}} = grep %$_,
-                                @{$rec->{related_material}->{record}};
-                            if (!$rec->{related_material}->{record}->[0]) {
-                                delete $rec->{related_material}->{record};
-                                if (!%{$rec->{related_material}}) {
-                                    delete $rec->{related_material};
-                                }
-                            }
-                            my $saved = h->main_publication->add($rec);
-                            h->main_publication->commit;
-                            h->publication->add($saved);
-                            h->publication->commit;
-                        }
-                    }
-                );
-
-                #return $return_hit;
-            }
+                    relation => $inverse_relation
+            };
         }
 
+        $target->{related_material}->{record} = \@new_relations;
+
+        my $saved = h->main_publication->add($target);
+        h->main_publication->commit;
+        h->publication->add($saved);
+        h->publication->commit;
+
+        $rm->{status} = $target->{status};
     }
 }
 
