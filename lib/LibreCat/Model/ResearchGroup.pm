32775aeca7d853de63b54b296e1d5f446b97a614
Vitali Peil <vitali.peil@uni-bielefeld.de>
2018-06-05 14:50:32 +0200

    add POD for model research group

--- lib/LibreCat/Model/ResearchGroup.pm
+++ lib/LibreCat/Model/ResearchGroup.pm
@@ -7,3 +7,30 @@ use namespace::clean;
 with 'LibreCat::Model';
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Model::ResearchGroup - a research group model
+
+=head1 SYNOPSIS
+
+    package MyPackage;
+
+    use LibreCat qw(research_group);
+
+    research_group->get(123);
+
+    my $rec = {...};
+    research_group->add($rec);
+
+    research_group->delete(123);
+
+=head1 SEE ALSO
+
+L<LibreCat>, L<LibreCat::Model>
+
+=cut
