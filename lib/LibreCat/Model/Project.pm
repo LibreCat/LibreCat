f3bd6bc25b4ad3c3ef801cf6e019a057617ada00
Vitali Peil <vitali.peil@uni-bielefeld.de>
2018-06-05 14:45:42 +0200

    add POD for model project

--- lib/LibreCat/Model/Project.pm
+++ lib/LibreCat/Model/Project.pm
@@ -7,3 +7,30 @@ use namespace::clean;
 with 'LibreCat::Model';
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Model::Project - a project model
+
+=head1 SYNOPSIS
+
+    package MyPackage;
+
+    use LibreCat qw(project);
+
+    project->get(123);
+
+    my $rec = {...};
+    project->add($rec);
+
+    project->delete(123);
+
+=head1 SEE ALSO
+
+L<LibreCat>, L<LibreCat::Model>
+
+=cut
