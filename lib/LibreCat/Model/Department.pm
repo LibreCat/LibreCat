3a0feb290aee5bda29ce1431766b44b1b54ec6f3
Vitali Peil <vitali.peil@uni-bielefeld.de>
2018-06-05 14:48:17 +0200

    add POD for model department

--- lib/LibreCat/Model/Department.pm
+++ lib/LibreCat/Model/Department.pm
@@ -7,3 +7,30 @@ use namespace::clean;
 with 'LibreCat::Model';
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Model::Deparment - a department model
+
+=head1 SYNOPSIS
+
+    package MyPackage;
+
+    use LibreCat qw(department);
+
+    department->get(123);
+
+    my $rec = {...};
+    department->add($rec);
+
+    department->delete(123);
+
+=head1 SEE ALSO
+
+L<LibreCat>, L<LibreCat::Model>
+
+=cut
