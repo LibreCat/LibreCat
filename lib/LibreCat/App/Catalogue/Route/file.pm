596cb1777ef387a9a98e6e39c46515ec506d70ed
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2018-08-30 13:45:15 +0000

    Creating a IO::Handle prototype for Plack writers

--- lib/LibreCat/App/Catalogue/Route/file.pm
+++ lib/LibreCat/App/Catalogue/Route/file.pm
@@ -53,8 +53,8 @@ sub _send_it {
                 my $content_type     = $file->{content_type};
                 my $http_status_code = 200;
 
-              # Tech.note: This is a hash of HTTP header/values, but the
-              #            function below requires an even-numbered array-ref.
+                # Tech.note: This is a hash of HTTP header/values, but the
+                #            function below requires an even-numbered array-ref.
                 my @http_headers = (
                     'Content-Type' => $content_type,
                     'Cache-Control' =>
@@ -62,11 +62,11 @@ sub _send_it {
                     'Pragma' => 'no-cache'
                 );
 
-         # Send the HTTP headers
-         # (back to either the user or the upstream HTTP web-server front-end)
+                # Send the HTTP headers
+                # (back to either the user or the upstream HTTP web-server front-end)
                 my $writer = $respond->([$http_status_code, \@http_headers]);
 
-                $files->stream($writer, $file);
+                $files->stream(h->io_from_plack_writer($writer), $file);
             },
         },
     );
