e24349b681393986b3be71f88a76b31c912c3c6d
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2019-01-23 13:38:25 +0000

    Harden the CrossRef exporter against network errors #564

--- lib/LibreCat/App/Catalogue/Route/importer.pm
+++ lib/LibreCat/App/Catalogue/Route/importer.pm
@@ -8,6 +8,7 @@ LibreCat::App::Catalogue::Route::importer - central handler for import routes
 
 use Catmandu::Sane;
 use Catmandu::Util;
+use Catmandu::Importer::getJSON;
 use LibreCat qw(publication);
 use Catmandu::Fix::trim as => 'trim';
 use Dancer ':syntax';
@@ -17,36 +18,53 @@ use URL::Encode qw(url_decode);
 sub _fetch_record {
     my ($id, $source) = @_;
 
-    return undef unless ($source =~ /^[a-zA-Z0-9]+$/);
+    eval {
+        return undef unless ($source =~ /^[a-zA-Z0-9]+$/);
 
-    # check agency: crossref or datacite
-    if ($source eq 'crossref') {
-        $id =~ s{^\D+[:\/]}{};
+        # check agency: crossref or datacite
+        if ($source eq 'crossref') {
+            $id =~ s{^\D+[:\/]}{};
 
-        my $data = Catmandu->importer(
-            'getJSON',
-            from => url_decode("https://api.crossref.org/works/$id/agency"),
-            timeout => 10,
-        )->first;
+            # Need to have an explicit Catmandu::Importer::getJSON
+            # new instance to have access to the 'warn=>0' features
+            # to switch of warning messages
+            my $data = Catmandu::Importer::getJSON->new(
+                'getJSON',
+                from    => url_decode("https://api.crossref.org/works/$id/agency"),
+                timeout => 10,
+                warn    => 0 ,
+            )->first;
 
-        if (   $data->{message}
-            && $data->{message}->{agency}->{id} eq "datacite")
-        {
-            $source = "datacite";
+            if (!$data) {
+                $source = "crossref";
+            }
+            elsif (   $data->{message}
+                && $data->{message}->{agency}->{id} eq "datacite")
+            {
+                $source = "datacite";
+            }
+            else {
+                $source = "crossref";
+            }
         }
-    }
 
-    my $pkg
-        = Catmandu::Util::require_package($source, 'LibreCat::FetchRecord');
+        my $pkg
+            = Catmandu::Util::require_package($source, 'LibreCat::FetchRecord');
 
-    unless ($pkg) {
-        h->log->error("failed to load LibreCat::FetchRecord::$source");
+        unless ($pkg) {
+            h->log->error("failed to load LibreCat::FetchRecord::$source");
+            return undef;
+        }
+
+        h->log->debug("Processing LibreCat::FetchRecord::$source $id");
+
+        return $pkg->new->fetch($id);
+    };
+    if ($@) {
+        debug "oops";
+        h->log->error("Failed to fetch $id from $source");
         return undef;
     }
-
-    h->log->debug("Processing LibreCat::FetchRecord::$source $id");
-
-    $pkg->new->fetch($id);
 }
 
 =head2 POST /librecat/record/import
@@ -69,54 +87,53 @@ post '/librecat/record/import' => sub {
         : $p->{data};
     my $source = $p->{source};
 
-    try {
-        my $imported_records = _fetch_record($p->{id} // $data, $source);
+    my $imported_records = _fetch_record($p->{id} // $data, $source);
 
-        die "no records imported" unless $imported_records;
-
-        for my $pub (@$imported_records) {
-            $pub->{_id}    = $bag->generate_id;
-            $pub->{status} = 'new'
-                ; # new is the status of records not checked by users/reviewers
-            $pub->{creator}
-                = {id => session->{user_id}, login => session->{user}};
-            $pub->{user_id}    = session->{user_id};
-            $pub->{department} = $user->{department};
-
-            if(h->config->{web_bulk_import} or !exists h->config->{web_bulk_import}){
-              # Use config/hooks.yml to register functions
-              # that should run before/after importing publications
-              h->hook('import-new-' . $source)->fix_around(
-                $pub,
-                sub {
-                    publication->add($pub);
-                }
-              );
-            }
-            else {
-              my $type = $pub->{type} || 'journal_article';
-              my $templatepath = "backend/forms";
-              $pub->{new_record} = 1;
-
-              return template $templatepath . "/$type.tt", $pub;
-            }
-        }
-
+    unless (Catmandu::Util::is_array_ref($imported_records)) {
         return template "backend/add_new",
             {
-            ok => "Imported "
-                . int(@$imported_records)
-                . " record(s) from $source",
-            imported => $imported_records
+            error    => "Import from $source failed - try later again" ,
+            imported => []
             };
     }
-    catch {
-        $id //= substr($data, 0, 40) . '...';
-        h->log->error("import failed: $_");
-        return template "backend/add_new",
-            {error => "Could not import $id from source $p->{source}."};
-    };
 
+    for my $pub (@$imported_records) {
+        $pub->{_id}    = $bag->generate_id;
+        $pub->{status} = 'new'
+            ; # new is the status of records not checked by users/reviewers
+        $pub->{creator}
+            = {id => session->{user_id}, login => session->{user}};
+        $pub->{user_id}    = session->{user_id};
+        $pub->{department} = $user->{department};
+
+        # If we allow bulk imports, add all the imported records
+        # otherwise return the first record
+        if(h->config->{web_bulk_import} or !exists h->config->{web_bulk_import}){
+          # Use config/hooks.yml to register functions
+          # that should run before/after importing publications
+          h->hook('import-new-' . $source)->fix_around(
+            $pub,
+            sub {
+                publication->add($pub);
+            }
+          );
+        }
+        else {
+          my $type = $pub->{type} || 'journal_article';
+          my $templatepath = "backend/forms";
+          $pub->{new_record} = 1;
+
+          return template $templatepath . "/$type.tt", $pub;
+        }
+    }
+
+    return template "backend/add_new",
+        {
+        ok => "Imported "
+            . int(@$imported_records)
+            . " record(s) from $source",
+        imported => $imported_records
+        };
 };
 
 1;
