04be80113f221220a4b2976ca711a2c11559f5a3
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2019-05-10 13:49:09 +0200

    token debugging

--- lib/LibreCat/Token.pm
+++ lib/LibreCat/Token.pm
@@ -5,19 +5,37 @@ use Moo;
 use Crypt::JWT ':all';
 use namespace::clean;
 
+with 'LibreCat::Logger';
+
 has secret => (is => 'ro', required => 1);
 
 sub encode {
     my ($self, $payload) = @_;
-    encode_jwt(payload => $payload // {}, key => $self->secret, alg => 'HS512');
+    $payload //= {};
+    my $token = encode_jwt(payload => $payload, key => $self->secret, alg => 'HS512');
+    if ($self->log->is_debug) {
+        $self->log->debugf("Encoded JWT token $token %s", $payload);
+    }
+    $token;
 }
 
 sub decode {
     my ($self, $token) = @_;
-    $token // return;
+    if ($self->log->is_debug && !defined $token) {
+        $self->log->debug("JWT token missing");
+        return;
+    }
     try {
-        decode_jwt(token => $token, key => $self->secret, accepted_alg => 'HS512');
+        my $payload = decode_jwt(token => $token, key => $self->secret, accepted_alg => 'HS512');
+        if ($self->log->is_debug) {
+            $self->log->debugf("Decoded JWT token $token %s", $payload);
+        }
+        $payload;
     } catch {
+        if ($self->log->is_debug) {
+            $self->log->debug("Invalid JWT token $token");
+        }
+        undef;
     };
 }
 
