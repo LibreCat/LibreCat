8c6b66fcf015bbe775b6f529dffd139c0308fb86
Vitali Peil <vitali.peil@uni-bielefeld.de>
2019-10-28 17:51:00 +0100

    first working of a mojo search api

--- lib/LibreCat/Controller/SearchApi.pm
+++ lib/LibreCat/Controller/SearchApi.pm
@@ -4,45 +4,58 @@ use Catmandu::Sane;
 use Catmandu::Util qw(:is);
 use LibreCat -self;
 use Mojo::Base "Mojolicious::Controller";
-use IO::Handle::Util;
-use IO::File;
-use URI::Escape qw(uri_escape_utf8);
 
 sub search {
     my $c     = $_[0];
     my $model = $c->param('model');
-    my $query    = $c->param('cql');
-    my $recs  = librecat->model($model) // return $c->not_found;
+    my $query = $c->param('cql');
 
-    my $hits = librecat->searcher->search($model, {
-            cql_query => $query,
-            start => $c->param('start') // 0,
-            limit => $c->param('limit') // 20,
+    # my $aggs = $c->param('aggs') ;
+    my $recs = librecat->model($model) // return $c->not_found;
 
+    my $hits = librecat->searcher->search(
+        $model,
+        {
+            q     => [$query],
+            start => $c->param('start'),
+            limit => $c->param('limit'),
         }
     );
 
     my $data = {
-        type => $model,
+        type  => $model,
         query => $query,
         count => $hits->total // 0,
-        attributes => {
-            hits => $hits->to_array,
-        },
+        attributes =>
+            {hits => $hits->to_array, aggs => $hits->{aggregations},},
         links => {self => $c->url_for->to_abs,},
-    }
+    };
 
     $c->render(json => {data => $data});
 }
 
+# this one is never reached !
 sub not_found {
     my $c     = $_[0];
     my $model = $c->param('model');
-    my $error = {
-        status => '404',
-        title  => "$model not found",
-    };
+    my $error = {status => '404', title => "$model not found",};
     $c->render(json => {errors => [$error]}, status => 404);
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+LibreCat::Controller::SearchApi - a model-specific search controller used by L<Mojolicious::Plugin::LibreCat::Api>
+
+=head1 SYNOPSIS
+
+=head2 SEE ALSO
+
+L<LibreCat>, L<Mojolicious::Plugin::LibreCat::Api>
+
+=cut
