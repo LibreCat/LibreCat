2e96a7c8f401fe4e9c23b243b029a5a3a2c1d9bb
Petra Kohorst <petra.kohorst@uni-bielefeld.de>
2020-09-23 10:19:07 +0200

    Make sure that only the current role is checked for permissions, not all roles in user account.

--- lib/LibreCat/Access.pm
+++ lib/LibreCat/Access.pm
@@ -46,7 +46,7 @@ sub by_user_id {
 #  then the department id of the publication should match
 #  the publication record
 sub by_user_role {
-    my ($self, $pub, $user) = @_;
+    my ($self, $pub, $user, $role) = @_;
 
     return 0 unless $pub && $user;
 
@@ -55,10 +55,11 @@ sub by_user_role {
 
     my $perm_by_user_role = $self->allowed_user_role // [];
 
-    for my $role (@$perm_by_user_role) {
-        next unless $user->{$role};
+    for my $urole (@$perm_by_user_role) {
+        next unless $user->{$urole};
+        next unless $role eq $urole;
 
-        if ($role eq 'reviewer') {
+        if ($urole eq 'reviewer') {
             for my $id (@{$user->{reviewer}}) {
                 for my $iid (@{$pub->{department} // []}) {
                     return 1 if $id->{_id} eq $iid->{_id};
@@ -68,14 +69,14 @@ sub by_user_role {
                 }
             }
         }
-        elsif ($role eq 'project_reviewer') {
+        elsif ($urole eq 'project_reviewer') {
             for my $id (@{$user->{project_reviewer}}) {
                 for my $iid (@{$pub->{project} // []}) {
                     return 1 if $id->{_id} eq $iid->{_id};
                 }
             }
         }
-        elsif ($role eq 'data_manager') {
+        elsif ($urole eq 'data_manager') {
             for my $id (@{$user->{data_manager}}) {
                 for my $iid (@{$pub->{department} // []}) {
                     return 1 if $id->{_id} eq $iid->{_id};
@@ -86,7 +87,7 @@ sub by_user_role {
                 }
             }
         }
-        elsif ($role eq 'delegate') {
+        elsif ($urole eq 'delegate') {
             my @user_ids = $self->all_user_ids($pub);
 
             for my $id (@{$user->{delegate}}) {
@@ -98,7 +99,7 @@ sub by_user_role {
             }
         }
         else {
-            $self->log->error("no role_permission_map for $role!");
+            $self->log->error("no role_permission_map for $urole!");
         }
     }
 
