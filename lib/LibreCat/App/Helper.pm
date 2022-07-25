fd993e968a1c642c015f061faa986c9a3dd662d5
Nicolas Franck <nicolas.franck@ugent.be>
2022-07-25 10:19:26 +0200

    do not insert user query into cql; q already added to q by h->extract_params

--- lib/LibreCat/App/Helper.pm
+++ lib/LibreCat/App/Helper.pm
@@ -194,7 +194,6 @@ sub is_marked {
 sub all_marked {
     my ($self) = @_;
     my $p = $self->extract_params();
-    push @{$p->{cql}}, $p->{q} if $p->{q};
     push @{$p->{cql}}, "status=public";
 
     my $hits       = librecat->searcher->search('publication', $p);
